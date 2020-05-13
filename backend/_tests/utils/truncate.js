adbf488ce8dc4d3079f58f36abc7fbc6773febdc
Bruno Zutim <bzutim@gmail.com>
2020-05-13 09:30:34 -0300

    refactor: change truncate method

--- backend/_tests/utils/truncate.js
+++ backend/_tests/utils/truncate.js
@@ -4,10 +4,11 @@ const sequelize = require('../../src/database/connection');
 module.exports = () => {
 
     return Promise.all(Object.keys(sequelize.models).map( async (key) => {
-
+        
         return [
-            sequelize.models[key].destroy({ truncate: true, force: true }),
-            sequelize.query(`DELETE FROM sqlite_sequence WHERE name = '${sequelize.models[key].tableName}'`)
+            sequelize.models[key].truncate({ cascade: true, restartIdentity: true }),
+            //sequelize.models[key].destroy({ truncate: true, force: true }),
+            //sequelize.query(`DELETE FROM sqlite_sequence WHERE name = '${sequelize.models[key].tableName}'`)
         ]
     }));
 }
