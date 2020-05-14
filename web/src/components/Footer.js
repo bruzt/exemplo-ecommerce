5b7ab69565c62f8ef36e5811e5513df0a4c87b59
Bruno Zutim <bzutim@gmail.com>
2020-05-14 20:06:29 -0300

    refactor: style

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import styled from 'styled-components';
 
 export default function Footer() {
 
-  return (
-    <Container>
+    return (
+        <>
+            <footer>
 
-        <p>Contruido por Bruno Zutim</p>
+                <p>Contruido por Bruno Zutim</p>
 
-    </Container>
-  );
+            </footer>
+
+            <style jsx>{`
+                footer {
+                    height: 200px;
+                    border-top: 1px solid black;
+                    bottom: 0;
+
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+            `}</style>
+        </>
+    );
 }
-
-const Container = styled.footer`
-    height: 200px;
-    border-top: 1px solid black;
-    bottom: 0;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,57 +1,85 @@
 import React from 'react';
-import styled from 'styled-components';
-import { FaShoppingBasket } from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
 import Link from 'next/link';
-
-import GlobalStyle from '../GlobalStyle';
 
 export default function Header() {
   
     return (
         <>
-            <GlobalStyle />
+            <style global jsx>{`
+                * {
+                    margin: 0;
+                    padding: 0;
+                    outline: 0;
+                    box-sizing: border-box;
+                }
+            
+                html, body {
+                    height: 100%;
+                }
+            
+                body {
+                    background: #99a19b;
+                    font-family: Arial, Helvetica, sans-serif;
+                }
+            
+                a {
+                    text-decoration: none;
+                    color: inherit;
+                }
+            `}</style>
 
-            <Container>
-                <div>
+            <header>
+                <div className='limit-center'>
                     <Link href='/'>
                         <img 
                             src='http://qnimate.com/wp-content/uploads/2014/03/images2.jpg'
                             alt='logo' 
+                            title='Home'
                         />
                     </Link>
 
-                    <Link href='/order'>
-                        <FaShoppingBasket className='icon' size={40} />
-                    </Link>
+                    <div className='icon' title='Carrinho de compras'>
+                        <Link href='/order'>
+                            <div>
+                                <FaShoppingCart size={40} />
+                                <p>Carrinho</p>
+                            </div>
+                        </Link>
+                    </div>
                 </div>
-            </Container>
+            </header>
+
+            <style jsx>{`
+                header {
+                    height: 100px;
+                    border-bottom: 1px solid black;
+                }
+
+                .limit-center {
+                    width: 100%;
+                    max-width: 1300px;
+                    margin: 0 auto;
+
+                    display: flex;
+                    justify-content: space-between;
+                }
+
+                img {
+                    max-width: 100%;
+                    height: 90px;
+                    align-self: center;
+                    cursor: pointer;
+                }
+
+                .icon {
+                    /*float: right;*/
+                    margin: 35px 10px 0 0;
+                    cursor: pointer;
+                    text-align: center;
+                }
+            
+            `}</style>
         </>
     );
 }
-
-const Container = styled.header`
-    height: 100px;
-    border-bottom: 1px solid black;
-
-    div {
-        width: 100%;
-        max-width: 1300px;
-        margin: 0 auto;
-
-        display: flex;
-        justify-content: space-between;
-    }
-
-    img {
-        max-width: 100%;
-        height: 90px;
-        align-self: center;
-        cursor: pointer;
-    }
-
-    .icon {
-        /*float: right;*/
-        margin: 55px 10px 0 0;
-        cursor: pointer;
-    }
-`;
