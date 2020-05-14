d9e2c1d0f7663ab4a8da4beccb2072056fa467fa
Bruno Zutim <bzutim@gmail.com>
2020-05-14 20:08:40 -0300

    refactor: set layout of main

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head'
-import styled from 'styled-components';
 import Link from 'next/link';
 
 import api from '../services/api';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import MainLayout from '../components/MainLayout';
 
 export default function Home() {
 
@@ -43,19 +43,23 @@ export default function Home() {
 
             <Header />
 
-            <Container>
+            <MainLayout>
 
-                <main>
+                <section>
                     <div className="p-grid">
 
                         {productsState.map( (product) => {
 
-                            const discount = (product.discount_percent != 0) ? product.discount_percent + '%' : null;
-                            const finalPrice = (product.discount_percent != 0) ? (product.price - (product.price * (product.discount_percent/100))).toFixed(2) : product.price;
+                            const discount = (product.discount_percent != 0) 
+                                ? '-' + product.discount_percent + '%' 
+                                : null;
+                            const finalPrice = (product.discount_percent != 0) 
+                                ? (product.price - (product.price * (product.discount_percent/100))).toFixed(2) 
+                                : Number(product.price).toFixed(2);
 
                             return (
-                                <div key={product.id} className='link'>
-                                    <Link href={`/product/${product.id}`}>
+                                <Link href={`/product/${product.id}`} key={product.id}>
+                                    <a title={product.name}>
                                         <div className='p-card'>
                                             <div className='img-container'>
                                                 <img 
@@ -64,83 +68,118 @@ export default function Home() {
                                                     alt={'imagem-' + product.name.split(' ').join('-')} 
                                                 />
                                             </div>
-                                            <p className='title'>{product.name}</p>
-                                            <div className={(product.discount_percent != 0) ? 'price-discount' : ''}>
-                                                <p className='price'>{finalPrice}</p>
-                                                <p className='discount'>{discount}</p>
+                                            <div className='title-price'>
+                                                <p className='title'>{product.name}</p>
+                                                <div className='price-discount'>
+                                                    <p className='price'>R$ {finalPrice}</p>
+                                                    {discount && <p className='discount'>{discount}</p>}
+                                                </div>
                                             </div>
-                                        </div>    
-                                    </Link>
-                                </div>
+                                        </div>   
+                                    </a>
+                                </Link>
                             )
                         })}
 
                     </div>
-                </main>
+                </section>
 
-            </Container>
+            </MainLayout>
 
             <Footer />
+
+            <style jsx>{`
+                section {
+                    min-height: 800px;
+                }
+
+                .p-grid {
+                    display: grid;
+                    grid-template-columns: 1fr 1fr 1fr 1fr;
+                    text-align: center;
+                    padding: 20px;
+                    grid-gap: 20px;
+                }
+
+                .p-grid .p-card {
+                    border: 1px solid black;
+                    border-radius: 5px;
+                    max-height: 350px;
+                    overflow: hidden;
+                    padding: 10px;
+                }
+
+                .p-grid .p-card img {
+                    width: 100%;
+                    max-height: 200px;
+                }
+
+                .p-grid .p-card .title-price {
+                    height: 100px;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                }
+
+                .p-grid .p-card .title {
+                    font-size: 15px;
+                    margin-top: 10px;
+                    
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    display: -webkit-box;
+                    -webkit-line-clamp: 2; /* number of lines to show */
+                    -webkit-box-orient: vertical;
+                }
+
+                .p-grid .p-card .price-discount {
+                    margin: 10px 0 0 0;
+                    display: flex;
+                    justify-content: space-around;
+                    align-items: center;
+                }
+
+                .p-grid .p-card .price-discount .discount {
+                    background: #41773A;
+                    padding: 5px 10px;
+                }
+
+                .p-grid .p-card .price {
+                    font-size: 30px;
+                    font-weight: bold;
+                    padding: 5px 10px;
+                }
+
+                @media (max-width: 1200px) {
+                    padding: 0;
+
+                    .p-grid {
+                        grid-template-columns: 1fr 1fr 1fr;
+                        padding: 10px;
+                        grid-gap: 10px;
+                    }
+                }
+
+                @media (max-width: 900px) {
+                    padding: 0;
+
+                    .p-grid {
+                        grid-template-columns: 1fr 1fr;
+                        padding: 5px;
+                        grid-gap: 5px;
+                    }
+                }
+
+                @media (max-width: 600px) {
+                    padding: 0;
+
+                    .p-grid {
+                        grid-template-columns: 1fr;
+                        padding: 0;
+                        grid-gap: 0;
+                    }
+                }
+            `}</style>
         </>
     )
 }
-
-const Container = styled.div`
-
-    main {
-        width: 100%;
-        max-width: 1300px;
-        min-height: 800px;
-        margin: 0 auto;
-        border-left: 1px solid black;
-        border-right: 1px solid black;
-    }
-
-    main .p-grid {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        text-align: center;
-        padding: 20px;
-        grid-gap: 20px;
-    }
-
-    main .link {
-        cursor: pointer;
-    }
-
-    main .p-grid .p-card {
-        border: 1px solid black;
-        border-radius: 5px;
-        height: 300px;
-        overflow: hidden;
-        padding: 10px;
-    }
-
-    main .p-grid .p-card .img-container {
-        width: 100%;
-        height: 200px;
-    }
-
-    main .p-grid .p-card img {
-        width: 100%;
-        height: 100%;
-    }
-
-    main .p-grid .p-card .title {
-        font-size: 25px;
-        margin-top: 10px;
-    }
-
-    main .p-grid .p-card .price-discount {
-        display: flex;
-        justify-content: space-around;
-    }
-
-    main .p-grid .p-card .price {
-        margin-top: 10px;
-    }
-
-    main .p-grid .p-card .discount {
-        margin-top: 10px;
-    }
-`;
