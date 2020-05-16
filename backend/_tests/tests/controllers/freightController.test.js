b8076f3af3671ad3fc4c74c7840dba045f88ddb5
Bruno Zutim <bzutim@gmail.com>
2020-05-16 09:34:36 -0300

    feat: freightController test suit done

---
+++ backend/_tests/tests/controllers/freightController.test.js
@@ -0,0 +1,24 @@
+const supertest = require('supertest');
+
+const app = require('../../../src/app');
+
+describe('freightController Test Suit', () => {
+
+    it('should show pac and sedex freight', async () => {
+
+        const response = await supertest(app).post(`/freight`)
+            .send({
+                destZipCode: "13490000",
+                weight: "0.300",
+                length: 10,
+                height: 10,
+                width: 10,
+                diameter: 10
+            });
+        
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("pac");
+        expect(response.body).toHaveProperty("sedex");
+    });
+
+});
