0915e09ed99eafa70a85545c02c414bfa7b0c80a
Bruno Zutim <bzutim@gmail.com>
2020-05-23 16:12:03 -0300

    refactor: change way to get freight, with timeout

--- backend/src/controllers/freightController.js
+++ backend/src/controllers/freightController.js
@@ -9,65 +9,92 @@ module.exports = {
 
         if(req.body.length < 15) req.body.length = 15;
 
+        let response, json, pac, sedex;
+        
         try {
-
-            let response = await axios.get(`https://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?sCepOrigem=13490000&sCepDestino=${req.body.destZipCode}&nVlPeso=${req.body.weight}&nCdFormato=1&nVlComprimento=${req.body.length}&nVlAltura=${req.body.height}&nVlLargura=${req.body.width}&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml`);
+            const source = axios.CancelToken.source();
             
-            let json = await parseStringPromise(response.data);
+            setTimeout(() => {
 
-            const pac = {
-                Valor: json.Servicos.cServico[0].Valor[0],
-                PrazoEntrega: json.Servicos.cServico[0].PrazoEntrega[0],
-                MsgErro: json.Servicos.cServico[0].MsgErro[0]
-            }
+                source.cancel();
 
-            response = await axios.get(`https://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?sCepOrigem=13490000&sCepDestino=${req.body.destZipCode}&nVlPeso=${req.body.weight}&nCdFormato=1&nVlComprimento=${req.body.length}&nVlAltura=${req.body.height}&nVlLargura=${req.body.width}&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04014&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3`);
+            }, 5000);
+            
+            response = await axios.get(`https://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?sCepOrigem=13490000&sCepDestino=${req.body.destZipCode}&nVlPeso=${req.body.weight}&nCdFormato=1&nVlComprimento=${req.body.length}&nVlAltura=${req.body.height}&nVlLargura=${req.body.width}&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml`, {
+                cancelToken: source.token
+            });
             
             json = await parseStringPromise(response.data);
 
-            const sedex = {
+            pac = {
                 Valor: json.Servicos.cServico[0].Valor[0],
                 PrazoEntrega: json.Servicos.cServico[0].PrazoEntrega[0],
                 MsgErro: json.Servicos.cServico[0].MsgErro[0]
             }
 
+        } catch (error) {
+            
+            pac = { message: 'Não foi possivel obter frete' }
+        }
+        
+        try {
 
-            /*
-            const [ pac ] = await correios.calcPrecoPrazo({
-                "nCdServico": "04510",                  // Código PAC
-                "sCepOrigem": "13490000",               // CEP ORIGEM (string sem hífen)
-                "sCepDestino": req.body.destZipCode,    // CEP DESTINO (string sem hífen)
-                "nVlPeso": req.body.weight,             // Peso em kilogramas (string: "0,500")
-                "nCdFormato": 1,                        // 1 – Formato caixa/pacote // 2 – Formato rolo/prisma // 3 – Envelope
-                "nVlComprimento": req.body.length,      // Comprimento em centimetros (float)
-                "nVlAltura": req.body.height,           // Altura em centimetros (float)
-                "nVlLargura": req.body.width,           // Largura em centimetros (float)
-                "nVlDiametro": 0,                       // Diametro em centimetros (float)
-                "sCdMaoPropria": "N",                   // Serviço adicional "mão própria" (string: "S" para sim, "N" para não)
-                "nVlValorDeclarado": 0,                 // Serviço adicional "valor declarado" (float: 0 para não)
-                "sCdAvisoRecebimento": "N"              // Serviço adicional "aviso de recebimento" (string: "S" para sim, "N" para não)
+            const source = axios.CancelToken.source();
+            
+            setTimeout(() => {
+
+                source.cancel();
+
+            }, 5000);
+        
+            response = await axios.get(`https://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?sCepOrigem=13490000&sCepDestino=${req.body.destZipCode}&nVlPeso=${req.body.weight}&nCdFormato=1&nVlComprimento=${req.body.length}&nVlAltura=${req.body.height}&nVlLargura=${req.body.width}&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04014&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3`, {
+                cancelToken: source.token
             });
-    
-            const [ sedex ] = await correios.calcPrecoPrazo({
-                "nCdServico": "04014",                  // Código SEDEX
-                "sCepOrigem": "13490000",
-                "sCepDestino": req.body.destZipCode,
-                "nVlPeso": req.body.weight,
-                "nCdFormato": 1,
-                "nVlComprimento": req.body.length,
-                "nVlAltura": req.body.height,
-                "nVlLargura": req.body.width,
-                "nVlDiametro": req.body.diameter,
-                "sCdMaoPropria": "N",
-                "nVlValorDeclarado": 0,
-                "sCdAvisoRecebimento": "N"
-            });*/
+            
+            json = await parseStringPromise(response.data);
 
-            return res.json({ pac, sedex });
+            sedex = {
+                Valor: json.Servicos.cServico[0].Valor[0],
+                PrazoEntrega: json.Servicos.cServico[0].PrazoEntrega[0],
+                MsgErro: json.Servicos.cServico[0].MsgErro[0]
+            }
 
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({ message: 'internal error' });
+            
+            sedex = { message: 'Não foi possivel obter frete' }
         }
+
+        /*
+        const [ pac ] = await correios.calcPrecoPrazo({
+            "nCdServico": "04510",                  // Código PAC
+            "sCepOrigem": "13490000",               // CEP ORIGEM (string sem hífen)
+            "sCepDestino": req.body.destZipCode,    // CEP DESTINO (string sem hífen)
+            "nVlPeso": req.body.weight,             // Peso em kilogramas (string: "0,500")
+            "nCdFormato": 1,                        // 1 – Formato caixa/pacote // 2 – Formato rolo/prisma // 3 – Envelope
+            "nVlComprimento": req.body.length,      // Comprimento em centimetros (float)
+            "nVlAltura": req.body.height,           // Altura em centimetros (float)
+            "nVlLargura": req.body.width,           // Largura em centimetros (float)
+            "nVlDiametro": 0,                       // Diametro em centimetros (float)
+            "sCdMaoPropria": "N",                   // Serviço adicional "mão própria" (string: "S" para sim, "N" para não)
+            "nVlValorDeclarado": 0,                 // Serviço adicional "valor declarado" (float: 0 para não)
+            "sCdAvisoRecebimento": "N"              // Serviço adicional "aviso de recebimento" (string: "S" para sim, "N" para não)
+        });
+
+        const [ sedex ] = await correios.calcPrecoPrazo({
+            "nCdServico": "04014",                  // Código SEDEX
+            "sCepOrigem": "13490000",
+            "sCepDestino": req.body.destZipCode,
+            "nVlPeso": req.body.weight,
+            "nCdFormato": 1,
+            "nVlComprimento": req.body.length,
+            "nVlAltura": req.body.height,
+            "nVlLargura": req.body.width,
+            "nVlDiametro": req.body.diameter,
+            "sCdMaoPropria": "N",
+            "nVlValorDeclarado": 0,
+            "sCdAvisoRecebimento": "N"
+        });*/
+
+        return res.json({ pac, sedex });
     }
 }
