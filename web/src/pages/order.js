c4324a7ae2d9b36ad8af457ec781c87a9181c078
Bruno Zutim <bzutim@gmail.com>
2020-05-25 19:46:05 -0300

    refactor: add ThanksForBuy page

--- web/src/pages/order.js
+++ web/src/pages/order.js
@@ -5,6 +5,7 @@ import { useOrder } from '../context/orderContext';
 import Cart from '../components/Cart';
 import SelectAddress from '../components/SelectAddress';
 import Payment from '../components/Payment';
+import ThanksForBuy from '../components/ThanksForBuy';
 
 export default function Order() {
 
@@ -19,4 +20,5 @@ export default function Order() {
     if(orderContext.order == 'cart') return <Cart />
     else if(orderContext.order == 'address') return <SelectAddress />
     else if(orderContext.order == 'payment') return <Payment />
+    else if(orderContext.order == 'thanksForBuy') return <ThanksForBuy />
 }
