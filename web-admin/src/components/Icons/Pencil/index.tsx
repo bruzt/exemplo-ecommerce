366484ea9db3eb4121d0f4d837035dd3fee44ad7
Bruno Zutim <bzutim@gmail.com>
2020-09-28 09:52:35 -0300

    create pencil and trash icons components

---
+++ web-admin/src/components/Icons/Pencil/index.tsx
@@ -0,0 +1,8 @@
+import React, { HTMLAttributes } from 'react';
+
+import { PencilIcon } from './styles';
+
+export default function Pencil(props: HTMLAttributes<SVGElement>){
+
+  return <PencilIcon {...props} />;
+}
