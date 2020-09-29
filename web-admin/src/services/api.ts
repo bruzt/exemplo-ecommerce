b5667d49c8b5e7274efce6896a2b66fe2781c451
Bruno Zutim <bzutim@gmail.com>
2020-09-29 10:05:23 -0300

    add admin bearer token to headers (provisional)

--- web-admin/src/services/api.ts
+++ web-admin/src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'http://localhost:3001',
+    headers: {
+        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWRtaW4iOnRydWUsImlhdCI6MTYwMTM4NDAxNSwiZXhwIjoxNjAxNDI3MjE1fQ.P2Frc71enY447r4RgUZbTmzbTY1i-mcBAqUNIhXSaBo'
+    }
 });
