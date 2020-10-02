0baf036d5ded0253b12422242b5eed8dcd149027
Bruno Zutim <bzutim@gmail.com>
2020-10-02 13:26:02 -0300

    change jwt (provisional)

--- web-admin/src/services/api.ts
+++ web-admin/src/services/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'http://localhost:3001',
     headers: {
-        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWRtaW4iOnRydWUsImlhdCI6MTYwMTU3MjY4MSwiZXhwIjoxNjAxNjE1ODgxfQ.JpElfBPKykRJ7_18UwRiQ8a27okAAVZn0IYxp2zKFaM'
+        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWRtaW4iOnRydWUsImlhdCI6MTYwMTY0MzU1MywiZXhwIjoxNjAxNjg2NzUzfQ.pMiWvkGOGKkOGGBSQXf4ZvKq47Uli-hv3GvT3GvtR_o'
     }
 });
