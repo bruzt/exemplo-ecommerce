9bbc60cf820f7c2168ba9ecb0366873136554e23
Bruno Zutim <bzutim@gmail.com>
2020-10-06 12:07:40 -0300

    add success (green) color to theme

--- web-admin/src/components/generic/Button/styles.ts
+++ web-admin/src/components/generic/Button/styles.ts
@@ -4,17 +4,17 @@ export const StyledButton = styled.button`
     width: 200px;
     height: 50px;
     font-size: 20px;
-    background: ${(props) => props.theme.secondary};
+    background: ${(props) => props.theme.success};
     color: ${(props) => props.theme.color};
     border: 0;
     border-radius: 4px;
     cursor: pointer;
 
     &:hover {
-        background: ${(props) => props.theme.background};
+        background: ${(props) => props.theme.successActive};
     }
 
     &:active {
-        background: ${(props) => props.theme.secondary};
+        background: ${(props) => props.theme.success};
     }
 `;
--- web-admin/src/styles/darkTheme.ts
+++ web-admin/src/styles/darkTheme.ts
@@ -4,6 +4,8 @@ export default {
     primary: '#0D2235',
     secondary: '#16324C',
     color: '#eee',
+    success: '#3E8C34',
+    successActive: '#41A933',
     warning: '#EED202',
     warningActive: '#f0dc4d',
     danger: '#a32e39',
