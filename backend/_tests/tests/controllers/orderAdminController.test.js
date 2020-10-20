189d0d66e33740233deb7e2f5aaa1f392dc601ae
Bruno Zutim <bzutim@gmail.com>
2020-10-20 09:34:11 -0300

    refactor: test of list orderAdminController done

--- backend/_tests/tests/controllers/orderAdminController.test.js
+++ backend/_tests/tests/controllers/orderAdminController.test.js
@@ -16,6 +16,36 @@ describe('orderController Test Suit', () => {
         //return truncate();
     });
 
+    it('it should list all orders', async () => {
+
+        const user = await factories.create('User');
+        const address = await factories.create('Address', { user_id: user.id });
+        const category = await factories.create('Category');
+        const product1 = await factories.create('Product', { category_id: category.id });
+        const product2 = await factories.create('Product', { category_id: category.id });
+        await factories.create('Order', { 
+            user_id: user.id, 
+            address_id: address.id, 
+            products_id: [product1.id]  
+        });
+        await factories.create('Order', { 
+            user_id: user.id, 
+            address_id: address.id, 
+            products_id: [product2.id]  
+        });
+
+        user.admin = true;
+        const token = user.generateToken();
+
+        const response = await supertest(app).get(`/admin/orders`)
+            .set('authorization', 'Bearer ' + token);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('count');
+        expect(response.body).toHaveProperty('orders');
+        expect(response.body.orders).toHaveLength(2);
+    });
+
     it('should update an order', async () => {
 
         const user = await factories.create('User');
