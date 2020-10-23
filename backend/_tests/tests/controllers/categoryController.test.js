b00813bf02ffe05956f97c9b691badba13105738
Bruno Zutim <bzutim@gmail.com>
2020-10-23 09:00:40 -0300

    feat: implemented delete category tests

--- backend/_tests/tests/controllers/categoryController.test.js
+++ backend/_tests/tests/controllers/categoryController.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest');
 const truncate = require('../../utils/truncate');
 const factories = require('../../utils/factories');
 const app = require('../../../src/app');
+const ProductModel = require('../../../src/models/ProductModel');
 
 describe('categoryController Test Suit', () => {
 
@@ -109,4 +110,79 @@ describe('categoryController Test Suit', () => {
         expect(response.body.message).toBe('no update has been made');
     });
 
+    it('should delete a category', async () => {
+
+        const user = await factories.create('User');
+        user.admin = true;
+        const token = user.generateToken();
+
+        const category1 = await factories.create('Category');
+        const category2 = await factories.create('Category', { name: 'categoria 2' });
+        const product = await factories.create('Product', { category_id: category1.id });
+
+        const response = await supertest(app).delete(`/categories/${category1.id}`)
+            .set('authorization', `Bearer ${token}`)
+            .send({
+                transferToId: category2.id
+            })
+        ;
+
+        const productAfter = await ProductModel.findByPk(product.id);
+
+        expect(response.status).toBe(204);
+        expect(productAfter.category_id).toBe(category2.id);
+    });
+
+    it('should return error 400 for "id and transferToId cannot be the same"', async () => {
+
+        const user = await factories.create('User');
+        user.admin = true;
+        const token = user.generateToken();
+        
+        const response = await supertest(app).delete(`/categories/1`)
+            .set('authorization', `Bearer ${token}`)
+            .send({
+                transferToId: 1
+            })
+        ;
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toBe("id and transferToId cannot be the same");
+    });
+
+    it('should return error 400 for "Category not found"', async () => {
+
+        const user = await factories.create('User');
+        user.admin = true;
+        const token = user.generateToken();
+        
+        const response = await supertest(app).delete(`/categories/1`)
+            .set('authorization', `Bearer ${token}`)
+            .send({
+                transferToId: 2
+            })
+        ;
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toBe("Category not found");
+    });
+
+    it('should return error 400 for "Category to transfer not found"', async () => {
+
+        const user = await factories.create('User');
+        user.admin = true;
+        const token = user.generateToken();
+
+        const category = await factories.create('Category');
+        
+        const response = await supertest(app).delete(`/categories/${category.id}`)
+            .set('authorization', `Bearer ${token}`)
+            .send({
+                transferToId: 2
+            })
+        ;
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toBe("Category to transfer not found");
+    });
 });
