c5bf98d174f7c93066d8e529deaadbfef51aa976
Bruno Zutim <bzutim@gmail.com>
2020-10-24 14:23:56 -0300

    feat: generate sitemap.xml

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,18 @@
+/*eslint no-undef: "off"*/
+
 const withImages = require('next-images');
 
 module.exports = withImages({
     env: {
-        BACKEND_URL: 'http://localhost:3001'
+        SITE_DOMAIN: process.env.SITE_DOMAIN || 'http://localhost:3000',
+        BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:3001'
+    },
+
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            require('./src/utils/generateSiteMap')
+        }
+    
+        return config
     }
 });
---
+++ web/src/utils/generateSiteMap.js
@@ -0,0 +1,45 @@
+/*eslint no-undef: "off"*/
+
+const axios = require('axios');
+const fs = require('fs');
+
+async function fetchProducts(){
+    try {
+
+        const response = await axios.get(`${process.env.BACKEND_URL}/products`);
+
+        return response.data.products;
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function generateSiteMap(){
+
+    const products = await fetchProducts();
+
+    const urls = products.map( (product) => (
+        `<url>
+            <loc>${`${process.env.SITE_DOMAIN}/${product.id}?product=${String(product.title).split(' ').join('-')}`}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <priority>0.6</priority>
+        </url>`
+    ));
+
+    const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url>
+                <loc>${process.env.SITE_DOMAIN}</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+                <priority>0.5</priority>
+            </url>
+            ${urls.join('')}
+        </urlset>
+    `;
+
+    fs.writeFileSync('public/sitemap.xml', sitemap);
+}
+
+generateSiteMap();
