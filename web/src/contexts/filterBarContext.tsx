878bb7b5c07949d0ed4f6bd5f0504944e57febe4
Bruno Zutim <bzutim@gmail.com>
2020-10-25 13:33:04 -0300

    refactor: filterBarContext to typescript

--- web/src/contexts/filterBarContext.tsx
+++ web/src/contexts/filterBarContext.tsx
@@ -3,32 +3,43 @@ import { useRouter } from 'next/router';
 
 import api from '../services/api';
 
+interface IProps {
+    children: React.ReactNode;
+}
+
+export interface ICategory {
+    id: number;
+    name: string;
+    parent_id: number;
+}
+
+interface IUseFilterBar {
+    getSearchBarText: string;
+    setSearchBarText: React.Dispatch<string>;
+    getCategories: ICategory[];
+}
+
 const Context = createContext({});
 
-export function FilterBarContextProvider({ children }){
+export function FilterBarContextProvider({ children }: IProps){
 
     const [getSearchBarText, setSearchBarText] = useState('');
-    const [getCategories, setCategories] = useState([]);
+    const [getCategories, setCategories] = useState<ICategory[]>([]);
 
     const router = useRouter();
 
     useEffect( () => {
-
         fetchCategories();
-
     }, []);
 
     useEffect( () => {
-
         if(router.route == '/' || router.query.categoryId){
 
             setSearchBarText('');
         }
-        
     }, [router.route, router.query]);
 
     async function fetchCategories(){
-
         try {
 
             const respose = await api.get('/categories');
@@ -53,7 +64,7 @@ export function FilterBarContextProvider({ children }){
 
 export function useFilterBar(){
 
-    const context = useContext(Context);
+    const context = useContext(Context) as IUseFilterBar;
 
     return context;
 }
