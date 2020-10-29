c775fe677c82a3b4bad871e99c8b9abc762348b3
Bruno Zutim <bzutim@gmail.com>
2020-10-29 20:19:40 -0300

    refactor: SelectAddress to typescript

--- web/src/components/SelectAddress/index.tsx
+++ web/src/components/SelectAddress/index.tsx
@@ -3,15 +3,17 @@ import { FaArrowLeft } from 'react-icons/fa';
 import Head from 'next/head';
 import axios from 'axios';
 
-import formatZipCode from '../utils/formatZipCode';
+import formatZipCode from '../../utils/formatZipCode';
 
-import { useUser } from '../contexts/userContext';
-import { useCart } from '../contexts/cartContext';
-import { useOrder } from '../contexts/orderContext';
+import { useUser } from '../../contexts/userContext';
+import { useCart } from '../../contexts/cartContext';
+import { useOrder } from '../../contexts/orderContext';
 
-export default function Address() {
+import { Container } from './styles';
 
-    const userContext = useUser();
+export default function SelectAddress() {
+	
+	const userContext = useUser();
     const cartContext = useCart();
     const orderContext = useOrder();
 
@@ -165,7 +167,7 @@ export default function Address() {
                 <meta name="robots" content="noindex" />
             </Head>
 
-            <section>
+            <Container>
 
                 <button 
                     type='button'
@@ -183,7 +185,14 @@ export default function Address() {
                     {(userContext.getUser.addresses) 
                         ? userContext.getUser.addresses.map( (address) => {
                                 return (
-                                    <div key={address.id} className={`addr-card ${(cartContext.getAddressId == address.id) ? 'selected' : ''}`}>
+									<div 
+										key={address.id} 
+										className={`
+											addr-card 
+											${(cartContext.getAddressId == address.id) ? 'selected' : ''} 
+											${(getDisabledGoToPaymentButton) ? 'disabled' : ''}
+										`}
+									>
                                         <div className='addr-data'>
                                             <div className='addr-remove'>
                                                 <button 
@@ -317,251 +326,7 @@ export default function Address() {
                     </form>
                 )}
                 
-
-            </section>
-
-            <style jsx>{`
-                section {
-                    min-height: 800px;
-                    padding: 20px 0;
-                }
-
-                .back-button {
-                    border: 0;
-                    background: transparent;
-                    font-size: 30px;
-                    cursor: pointer;
-                    color: inherit;
-                }
-
-                h1 {
-                    text-align: center;
-                    margin: 20px;
-                }
-
-                .addr-grid {
-                    display: grid;
-                    grid-template-columns: 1fr 1fr;
-                    grid-gap: 10px;
-
-                    min-height: 220px;
-                    border: 1px solid #0D2235;
-                    border-radius: 5px;
-                    padding: 10px;
-                }
-
-                .addr-grid a {
-                    cursor: pointer;
-                }
-
-                .addr-card {
-                    border-radius: 5px;
-                    background: #0D2235;
-                    height: 220px;
-                }
-
-                .selected {
-                    border: 3px solid ${(getDisabledGoToPaymentButton) ? '#a32e39' : '#3E8C34'}
-                }
-
-                .addr-card .addr-data a div {
-                    padding: 15px;
-                    line-height: 25px;
-                    height: 100%;
-                }
-
-                .addr-card .addr-data .addr-remove {
-                    display: flex;
-                    justify-content: flex-end;
-                    margin: 10px 10px 0 0;
-                }
-
-                .addr-data .addr-remove button {
-                    font-weight: bold;
-                    padding: 4px 8px;
-                    border: 0;
-                    border-radius: 5px;
-                    background: #a32e39;
-                    cursor: pointer;
-                }
-
-                .addr-data .addr-remove button:hover {
-                    background: #bf2232;
-                }
-
-                .addr-data .addr-remove button:active {
-                    background: #a32e39;
-                }
-
-                .addr-data p {
-                    font-size: 20px;
-                    line-height: 30px;
-
-                    overflow: hidden;
-                    text-overflow: ellipsis;
-                    display: -webkit-box;
-                    -webkit-line-clamp: 1;
-                    -webkit-box-orient: vertical;
-                }
-
-                .add-select-buttons {
-                    display: flex;
-                    justify-content: space-between;
-                }
-
-                .add-select-buttons button {
-                    margin: 20px 0 0 0;
-                    width: 200px;
-                    height: 50px;
-                    border: 0;
-                    border-radius: 5px;
-                    font-size: 20px;
-                    cursor: pointer;                 
-                    color: inherit;   
-                }
-
-                .add-select-buttons .add-button {
-                    background: #0D2235;
-                }
-
-                .add-select-buttons .add-button:hover {
-                    background: #16324C;
-                }
-
-                .add-select-buttons .add-button:active {
-                    background: #0D2235;
-                }
-                
-                .add-select-buttons .select-button {
-                    background: #3E8C34;
-                }
-
-                .add-select-buttons .select-button:hover {
-                    background: #41A933;
-                }
-
-                .add-select-buttons .select-button:active {
-                    background: #3E8C34;
-                }
-
-                .add-select-buttons .select-button:disabled {
-                    background: #a32e39;
-                    color: inherit;
-                }
-
-                .add-select-buttons .select-button:disabled:hover {
-                    background: #bf2232;
-                }
-
-                .add-addr-form {
-                    display: flex;
-                    flex-direction: column;
-                    margin: 20px 0 0 0;
-                    border-radius: 5px;
-                    padding: 20px;
-                    width: 480px;
-                    max-width: 50%;
-                    background: #0D2235;
-                }
-
-                .flex-row {
-                    display: flex;
-                    flex-direction: row;
-                    width: 100%;
-                }
-
-                .flex-row input {
-                    margin: 0 5px;
-                }
-
-                .flex-column {
-                    display: flex;
-                    flex-direction: column;
-                    
-                }
-
-                .add-addr-form div {
-                    margin: 0 0 10px 0;
-                }
-
-                .add-addr-form label {
-                    margin: 0 0 0 5px;
-                }
-
-                .add-addr-form input, select {
-                    height: 30px;
-                    padding: 3px;
-                    font-size: 20px;
-                    border: 0;
-                    border-radius: 5px;
-                }
-
-                .add-addr-form #street {
-                    width: 100%;   
-                }
-
-                .add-addr-form #number {
-                    width: 100%;
-                    max-width: 150px;
-                }
-
-                .add-addr-form #district {
-                    width: 100%;
-                }
-
-                .add-addr-form #city {
-                    width: 240px;
-                    margin-right: 5px;
-                }
-
-                .add-addr-form #state {
-                    width: 100%;
-                    min-width: 65px;   
-                    background: #fff;
-                }
-
-                .add-addr-form #zipcode {
-                    width: 100%;
-                    text-align: center;
-                }
-
-                .addr-submit {
-                    width: 100%; 
-                    height: 40px;
-                    margin-top: 10px;
-                    align-self: center;
-                    border: 0;
-                    border-radius: 5px;
-                    background: ${(getDisableAddAddrButton) ? '#a32e39' : '#3E8C34'};
-                    font-size: 20px;
-                    cursor: pointer;
-                    color: inherit;
-                }
-
-                .addr-submit:hover {
-                    background: ${(getDisableAddAddrButton) ? '#bf2232' : '#41A933'};
-                }
-
-                .addr-submit:active {
-                    background: #3E8C34;
-                }
-
-                .go-payment-container {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: flex-end;
-                }
-
-                @media (max-width: 425px) {
-                    .addr-grid {
-                        grid-template-columns: 1fr;
-                    }
-
-                    .add-addr-form {
-                        max-width: 100%;
-                    }
-                }
-            `}</style>
-        </>
-    );
+            </Container>
+		</>
+	);
 }
