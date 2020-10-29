1ceb4fbc3f0379a324a6b30a661a88739620c618
Bruno Zutim <bzutim@gmail.com>
2020-10-29 18:52:38 -0300

    refactor: Footer to typescript

---
+++ web/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Footer() {
+
+    return (
+        <Container>
+
+            <p>Contruido por Bruno Zutim</p>
+
+        </Container>
+    );
+}
---
+++ web/src/components/Footer/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+    height: 200px;
+    bottom: 0;
+    background: #0D2235;
+    color: #eee;
+    margin: 5px 0 0 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
