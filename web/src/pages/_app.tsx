78b81621fc20d23583f566a17847122f61fe811d
Bruno Zutim <bzutim@gmail.com>
2020-10-30 20:04:14 -0300

    refactor: change css import from _app to imageSlider component

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
 import { UserContextProvider } from '../contexts/userContext';
 import { OrderContextProvider } from '../contexts/orderContext';
 import { CartContextProvider } from '../contexts/cartContext';
