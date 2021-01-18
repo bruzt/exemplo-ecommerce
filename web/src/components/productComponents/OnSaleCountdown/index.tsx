7ce4112ddb1153542b8b92e3ad438ce7a8eb7eab
Bruno Zutim <bzutim@gmail.com>
2021-01-18 16:33:34 -0300

    refactor: calc dateNow based in milliseconds

--- web/src/components/productComponents/OnSaleCountdown/index.tsx
+++ web/src/components/productComponents/OnSaleCountdown/index.tsx
@@ -14,19 +14,19 @@ interface IProps {
 export default function OnSaleCountdown({ product, setIsOnSale, timeoutId }: IProps) {
 
 	const [getDays, setDays] = useState(0);
-    const [getHours, setHours] = useState(0);
-    const [getMinutes, setMinutes] = useState(0);
+	const [getHours, setHours] = useState(0);
+	const [getMinutes, setMinutes] = useState(0);
 	const [getSeconds, setSeconds] = useState(0);
-	
-	useEffect( () => {
+
+	useEffect(() => {
 		calcCountdown();
 
 		return () => clearTimeout(timeoutId);
 	}, []);
 
-	function calcCountdown(){
-		
-		if(product.isOnSale){
+	function calcCountdown() {
+
+		if (product.isOnSale) {
 
 			const startDate = new Date(product.discount_datetime_start);
 			const endDate = new Date(product.discount_datetime_end);
@@ -38,32 +38,32 @@ export default function OnSaleCountdown({ product, setIsOnSale, timeoutId }: IPr
 			loopCountdown(milliseconds, startDate, endDate);
 		}
 	}
-	
-	function loopCountdown(milliseconds: number, startDate: Date, endDate: Date){
 
-		const dateNow = new Date(product.dateNow);
-		
-		if(startDate <= dateNow && endDate >= dateNow) {
+	function loopCountdown(milliseconds: number, startDate: Date, endDate: Date) {
+
+		const dateNow = new Date(-milliseconds + Number(startDate));
+
+		if (startDate <= dateNow && endDate >= dateNow) {
 
 			const totalSeconds = -milliseconds / 1000;
 			setSeconds(Math.floor(totalSeconds % 60));
-	
+
 			const totalMinutes = totalSeconds / 60;
 			setMinutes(Math.floor(totalMinutes % 60));
-	
+
 			const totalHours = totalMinutes / 60;
 			setHours(Math.floor(totalHours % 24));
-	
+
 			const totalDays = totalHours / 24;
 			setDays(Math.floor(totalDays));
-	
+
 			milliseconds += 1000;
 
-		} 
-        else return setIsOnSale(false);
+		}
+		else return setIsOnSale(false);
 
-		timeoutId = setTimeout( () => {
-				
+		timeoutId = setTimeout(() => {
+
 			loopCountdown(milliseconds, startDate, endDate);
 
 		}, 1000);
@@ -71,14 +71,14 @@ export default function OnSaleCountdown({ product, setIsOnSale, timeoutId }: IPr
 
 	return (
 		<Container>
-			
+
 			<div className="countdown">
 				<FaClock size={25} />
 				<span>{getDays} Dia{getDays > 1 ? 's' : ''} {getHours < 10 ? '0' + getHours : getHours}:{getMinutes < 10 ? '0' + getMinutes : getMinutes}:{getSeconds < 10 ? '0' + getSeconds : getSeconds}</span>
 			</div>
 
-			<hr/>
-			
+			<hr />
+
 		</Container>
 	);
 }
