5a65abe8dfc618773a3e8e4494439ec5ca5ee402
Bruno Zutim <bzutim@gmail.com>
2021-02-16 09:13:02 -0300

    refactor: sonic connect

--- backend/src/database/sonic/connection.js
+++ backend/src/database/sonic/connection.js
@@ -2,15 +2,15 @@ const Sonic = require('sonic-channel');
 
 const ingest = new Sonic.Ingest({
 	host: process.env.SONIC_HOST,
-	port: process.env.SONIC_PORT,
+	port: Number(process.env.SONIC_PORT),
 	auth: process.env.SONIC_AUTH,
-});
+}).connect();
 
 const search = new Sonic.Search({
 	host: process.env.SONIC_HOST,
-	port: process.env.SONIC_PORT,
+	port: Number(process.env.SONIC_PORT),
 	auth: process.env.SONIC_AUTH,
-});
+}).connect();
 
 module.exports = {
 	ingest,
