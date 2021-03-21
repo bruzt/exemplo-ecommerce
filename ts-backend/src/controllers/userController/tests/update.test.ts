6e5a7d608e65d0e5a1b7de25ba918a8b57ec928d
Bruno Zutim <bzutim@gmail.com>
2021-03-21 10:06:58 -0300

    refactor: user update tests

--- ts-backend/src/controllers/userController/tests/update.test.ts
+++ ts-backend/src/controllers/userController/tests/update.test.ts
@@ -38,12 +38,60 @@ describe('userController Update Test Suit', () => {
         const response = await supertest(app).put('/users')
             .set('authorization', 'Bearer ' + token)
             .send({
-                name: 'test'
+                name: 'test',
+                email: 'other@test.br',
+                cpf: '71314297082',
             });
         
         expect(response.status).toBe(200);
         expect(response.body.name).toBe('test');
     });
+
+    it('should update the password of a user', async () => {
+
+        const user = UserModel.create(fakeUser);
+        await user.save();
+        const token = user.generateJwt();
+
+        const response = await supertest(app).put('/users')
+            .set('authorization', 'Bearer ' + token)
+            .send({
+                currentPassword: '123456',
+                newPassword: '123457',
+            });
+        
+        expect(response.status).toBe(200);
+    });
+
+    it('should not update the password for "wrong current password"', async () => {
+
+        const user = UserModel.create(fakeUser);
+        await user.save();
+        const token = user.generateJwt();
+
+        const response = await supertest(app).put('/users')
+            .set('authorization', 'Bearer ' + token)
+            .send({
+                currentPassword: '123457',
+                newPassword: '123458',
+            });
+        
+        expect(response.status).toBe(406);
+    });
+
+    it('should return erro for "invalid cpf"', async () => {
+
+        const user = UserModel.create(fakeUser);
+        await user.save();
+        const token = user.generateJwt();
+
+        const response = await supertest(app).put('/users')
+            .set('authorization', 'Bearer ' + token)
+            .send({ cpf: '96575214538' });
+        
+        expect(response.status).toBe(406);
+        expect(response.body.message).toBe('invalid cpf');
+    });    
 
     it('should return error for "user not found"', async () => {
 
@@ -54,11 +102,10 @@ describe('userController Update Test Suit', () => {
         const response = await supertest(app).put('/users')
             .set('authorization', 'Bearer ' + token)
             .send({
-                name: 'test'
+                name: 'test',
             });
         
         expect(response.status).toBe(404);
         expect(response.body.message).toBe("user not found");
     });
-    
 });
