1425dddfddb8845067f043050a9bcd371f5a47a7
Bruno Zutim <bzutim@gmail.com>
2021-03-22 10:27:46 -0300

    refactor: return array of erros

--- ts-backend/src/middlewares/celebrateCustomErrors.ts
+++ ts-backend/src/middlewares/celebrateCustomErrors.ts
@@ -11,35 +11,39 @@ export default (err: CelebrateError | Error, req: Request, res: Response, next: 
 
 		if(headersDetails){
 
-			const detail = headersDetails[0];
-			let status: number;
-			
-			if(detail.context?.key == 'authorization') status = 401;
-			else status = 400;
+			const messages = headersDetails.map( (detail) => {
+				return detail.message;
+			});
+
+			const status = headersDetails[0].context?.key == 'authorization' ? 401 : 400;
 
 			return res.status(status).json({ 
 				source: 'headers',
-				message: detail.message 
+				message: messages 
 			});
 		}
 
 		else if(paramsDetails){
 
-			const detail = paramsDetails[0];
+			const messages = paramsDetails.map( (detail) => {
+				return detail.message;
+			});
 			
 			return res.status(400).json({ 
 				source: 'params',
-				message: detail.message 
+				message: messages 
 			});
 		}
 
 		else if(bodyDetails){
 
-			const detail = bodyDetails[0];
+			const messages = bodyDetails.map( (detail) => {
+				return detail.message;
+			});
 
 			return res.status(400).json({ 
 				source: 'body',
-				message: detail.message 
+				message: messages
 			});
 		}
 	}
