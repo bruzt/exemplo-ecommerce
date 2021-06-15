9db5a6130ec376ad1bf37d71243a5fa1167869c2
Bruno Zutim <bzutim@gmail.com>
2021-06-15 09:19:14 -0300

    db connect with url

--- backend/src/config/databaseConfig.js
+++ backend/src/config/databaseConfig.js
@@ -9,11 +9,7 @@ require('dotenv').config({
 });
 
 module.exports = {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    dialect: process.env.DATABASE_DIALECT,
     storage: (process.env.NODE_ENV === 'test') ? './database.sqlite' : undefined,
     define: {
         timestamps: true,
