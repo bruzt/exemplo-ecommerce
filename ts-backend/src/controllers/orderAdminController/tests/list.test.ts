63e73c166dd37c2e5eeb2b5df1514eaa89f3b3c1
Bruno Zutim <bzutim@gmail.com>
2021-06-30 10:27:07 -0300

    refactor: add test to orderAdminController

--- ts-backend/src/controllers/orderAdminController/tests/list.test.ts
+++ ts-backend/src/controllers/orderAdminController/tests/list.test.ts
@@ -7,6 +7,9 @@ import app from '../../../app';
 import UserModel from '../../../models/UserModel';
 import AddressModel from '../../../models/AddressModel';
 import OrderModel from '../../../models/OrderModel';
+import CategoryModel from '../../../models/CategoryModel';
+import ProductModel from '../../../models/ProductModel';
+import OrderProductModel from '../../../models/OrderProductModel';
 
 const fakeUser = {
     name: "fake user",
@@ -30,6 +33,19 @@ const fakeOrder = {
     total_price: 30.00,
     payment_method: 'credit_card',
     status: 'paid',
+}
+
+const fakeProduct = {
+    title: 'fake product',
+    description: 'bla bla bla',
+    html_body: '<p>vai</p>',
+    price: "10.50",
+    quantity_stock: 100,
+    tangible: true,
+    weight: "5",
+    length: 15,
+    height: 15,
+    width: 15,
 }
 
 describe('orderAdminController List Test Suit', () => {
@@ -84,5 +100,58 @@ describe('orderAdminController List Test Suit', () => {
         expect(response.body).toHaveProperty('count');
         expect(response.body).toHaveProperty('orders');
         expect(response.body.orders).toHaveLength(2);
+    });
+
+    it('it should list all orders, filtered by status and offset', async () => {
+
+        const user =  UserModel.create(fakeUser);
+        user.admin = true;
+        await user.save();
+        const token = user.generateJwt();
+
+        const address =  AddressModel.create({ ...fakeAddress, user_id: user.id });
+        await address.save();
+
+        const order1 = OrderModel.create({ 
+            ...fakeOrder,
+            user_id: user.id, 
+            address_id: address.id, 
+        });
+        await order1.save();
+
+        const order2 = OrderModel.create({ 
+            ...fakeOrder,
+            user_id: user.id, 
+            address_id: address.id, 
+        });
+        await order2.save();
+
+        const category = CategoryModel.create({
+            name: 'teste',
+        });
+        await category.save();
+
+        const product = ProductModel.create({
+            ...fakeProduct,
+            category,
+        });
+        await product.save();
+
+        const orderProduct = OrderProductModel.create({
+            order: order1,
+            product,
+            quantity_buyed: 1,
+            product_price: 15,
+        });
+        await orderProduct.save();
+
+        const response = await supertest(app).get(`/admin/orders?limit=15&offset=1&status=paid`)
+            .set('authorization', 'Bearer ' + token)
+        ;
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('count');
+        expect(response.body).toHaveProperty('orders');
+        expect(response.body.orders).toHaveLength(1);
     });
 });
