5cb5375a99b440282a395b09f99c281773a2292f
Bruno Zutim <bzutim@gmail.com>
2021-07-08 11:19:31 -0300

    feat: open bigger modal when click on product image

--- web/src/components/productComponents/ImageSlider/index.tsx
+++ web/src/components/productComponents/ImageSlider/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from 'react-responsive-carousel';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import { Container } from './styles';
+import { Container, ImgModal, FaTimesStyled } from './styles';
 
 import { IImage } from '../../../pages/[productId]';
 
@@ -11,20 +12,84 @@ interface IProps {
 }
 
 export default function ImageSlider({ images }: IProps) {
+
+    const [modalActiveIndex, setModalActiveIndex] = useState<number | null>(null);
+
+    function handleAdvanceModalActiveImage(){
+
+        if(modalActiveIndex == images.length - 1) {
+
+            setModalActiveIndex(0);
+
+        } else {
+
+            setModalActiveIndex(modalActiveIndex + 1);
+        }
+    }
+
+    function handleRegressModalActiveImage(){
+
+        if(modalActiveIndex == 0) {
+
+            setModalActiveIndex(images.length - 1);
+
+        } else {
+
+            setModalActiveIndex(modalActiveIndex - 1);
+        }
+    }
 	
-	const customRenderThumb = (children) => children.map( (item, index) => (
+	/*const customRenderThumb = (children) => children.map( (item, index) => (
 		<div key={index} className="thumb-container">
 			<img src={item.props.src} alt={item.props.alt} /> 
 		</div>
-    ));
-        
+    ));*/
+
+        if(process.browser) console.log(window.innerWidth)  
+
+
     return (
         <Container>
+
+            {(process.browser && (window.innerWidth > 720) && (modalActiveIndex !== null)) && (
+                <ImgModal>
+                    <figure>
+
+                        <button
+                            type='button'
+                            onClick={() => setModalActiveIndex(null)}
+                            className='close'
+                        >
+                            <FaTimesStyled />
+                        </button>
+
+                        <img src={`${process.env.BACKEND_URL}/uploads/${images[modalActiveIndex].filename}`} alt='imagem' />
+
+                        <button
+                            type='button'
+                            className='left'
+                            onClick={handleRegressModalActiveImage}
+                        >
+                            <AiOutlineLeft size='2rem' color='#111' />
+                        </button>
+
+                        <button
+                            type='button'
+                            className='right'
+                            onClick={handleAdvanceModalActiveImage}
+                        >
+                            <AiOutlineRight size='2rem' color='#111' />
+                        </button>
+
+                    </figure>
+                </ImgModal>
+            )}
+
             <div className="img-container">
 
                 <Carousel
                     className='carousel'
-                    renderThumbs={customRenderThumb}
+                    //renderThumbs={customRenderThumb}
                     autoPlay={true}
                     stopOnHover={true}
                     infiniteLoop={true}
@@ -34,7 +99,18 @@ export default function ImageSlider({ images }: IProps) {
                     {(images.length > 0)
                         //? images.map( (image, index) => <img src={image.url} key={index} />)
                         //: testImages.map((image, index) => <img src={image.url} key={index} />)
-                        ? images.map( (image, index) => <img src={`${process.env.BACKEND_URL}/uploads/${image.filename}`} key={index} alt={image.filename} />)
+                        ? images.map( (image, index) => (
+                            <button 
+                                type='button'
+                                key={image.id}
+                                onClick={() => setModalActiveIndex(index)}
+                            >
+                                <img 
+                                    src={`${process.env.BACKEND_URL}/uploads/${image.filename}`} 
+                                    alt={image.filename} 
+                                />
+                           </button>
+                        ))
                         : [1].map( (i) => <img src='/images/img-n-disp.png' key={i} />) 
                     }
                 </Carousel>
--- web/src/components/productComponents/ImageSlider/styles.ts
+++ web/src/components/productComponents/ImageSlider/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { FaTimes } from 'react-icons/fa'
 
 export const Container = styled.div`
 
@@ -14,6 +15,18 @@ export const Container = styled.div`
         z-index: 0;
     }
 
+    li.slide button {
+        width: fit-content;
+        height: fit-content;
+
+        background: transparent;
+        border: 0;
+
+        cursor: pointer;
+
+        z-index: -1;
+    }
+
     .img-container img {
         width: auto;
         max-width: 43.75rem;
@@ -21,15 +34,25 @@ export const Container = styled.div`
         max-height: 20.625rem;
     }
 
-    .thumb-container {
-        width: 4.375rem;
-        height: 2.5rem;
+    .thumb {
+        width: 4.775rem;
+        height: 2.95rem;
 
-        background: #0D2235;
+        border: 2px solid transparent;
+
+        cursor: pointer;
+
+        &.selected,
+        &.selected:hover {
+            border: 2px solid ${props => props.theme.background};
+        }
+
+        &:hover {
+            border: 2px solid transparent;
+        }
     }
 
-    .thumb-container img {
-        
+    .thumb img {
         width: auto;
         max-width: 4.375rem;
         height: auto;
@@ -38,4 +61,74 @@ export const Container = styled.div`
         display: block;
         margin: 0 auto;
     }
+`;
+
+export const ImgModal = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    z-index: 100;
+
+    background: rgba(1,1,1,0.5);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    figure {
+        width: 90vw;
+        height: 90vh;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        position: relative;
+    }
+
+    img {
+        width: auto;
+        max-width: 100%;
+        height: auto;
+        max-height: 100%;
+    }
+
+    figure button {
+        width: 3rem;
+        height: 3rem;
+
+        position: absolute;
+        font-size: 0;
+
+        background: #eee;
+        border: 0;
+        border-radius: 0.25rem;
+
+        z-index: 10;
+    }
+
+    figure button.close {
+        top: 5%;
+        right: 0;
+    }
+
+    figure button.left {
+        top: 50%;
+        left: 5%;
+    }
+
+    figure button.right {
+        top: 50%;
+        right: 5%;
+    }
+`;
+
+export const FaTimesStyled = styled(FaTimes)`
+    width: 2rem;
+    height: 2rem;
+
+    color: ${props => props.theme.danger};
 `;
