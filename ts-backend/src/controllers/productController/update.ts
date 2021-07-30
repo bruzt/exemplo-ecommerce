aeed49e9ae3882abf31df473eebc6671f78d4ad1
Bruno Zutim <bzutim@gmail.com>
2021-07-30 16:04:10 -0300

    refactor: bugfix when value to update is 0

--- ts-backend/src/controllers/productController/update.ts
+++ ts-backend/src/controllers/productController/update.ts
@@ -51,16 +51,16 @@ export default async function update(req: Request, res: Response) {
         if(body.description) product.description = body.description;
         if(body.discount_datetime_start) product.discount_datetime_start = body.discount_datetime_start;
         if(body.discount_datetime_end) product.discount_datetime_end = body.discount_datetime_end;
-        if(body.discount_percent) product.discount_percent = body.discount_percent;
+        if(body.discount_percent != null) product.discount_percent = body.discount_percent;
         if(body.html_body) product.html_body = body.html_body;
-        if(body.price) product.price = String(body.price);
-        if(body.quantity_sold) product.quantity_sold = body.quantity_sold;
-        if(body.quantity_stock) product.quantity_stock = body.quantity_stock;
+        if(body.price != null) product.price = String(body.price);
+        if(body.quantity_sold != null) product.quantity_sold = body.quantity_sold;
+        if(body.quantity_stock != null) product.quantity_stock = body.quantity_stock;
         if(body.tangible) product.tangible = body.tangible;
-        if(body.weight) product.weight = body.weight;
-        if(body.height) product.height = body.height;
-        if(body.width) product.width = body.width;
-        if(body.length) product.length = body.length;
+        if(body.weight != null) product.weight = body.weight;
+        if(body.height != null) product.height = body.height;
+        if(body.width != null) product.width = body.width;
+        if(body.length != null) product.length = body.length;
         
         await product.save();
 
