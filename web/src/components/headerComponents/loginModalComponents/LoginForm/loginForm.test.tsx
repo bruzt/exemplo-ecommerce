87d2bffc58e11b0ace940dc11f17ab2413fe7638
Bruno Zutim <bzutim@gmail.com>
2021-08-15 11:34:40 -0300

    refactor: login form tests

---
+++ web/src/components/headerComponents/loginModalComponents/LoginForm/loginForm.test.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MockAdapter from 'axios-mock-adapter';
+
+import LoginForm from './';
+import { UserContextProvider } from '../../../../contexts/userContext';
+import api from '../../../../services/api';
+import { fakeUser, fakeUserJwt } from '../../../../testUtils/fakeData';
+
+    /*beforeAll(() => {
+        const apiMock = new MockAdapter(api);
+        apiMock.onPost('/sessions').reply(200, { token: fakeUserJwt });
+        apiMock.onGet('/users/1').reply(200, fakeUser);
+        apiMock.onPost('/users').reply(201, { token: fakeUserJwt });
+    });*/
+
+
+describe('Login Form Tests', () => {
+
+    it('should log in', async () => {
+
+        const apiMock = new MockAdapter(api);
+        apiMock.onPost('/sessions').reply(200, { token: fakeUserJwt });
+        apiMock.onGet('/users/1').reply(200, fakeUser);
+
+        const spyApi = jest.spyOn(api, 'post');
+
+        const { getByTestId } = await waitFor(() => render(
+            <UserContextProvider>
+                <LoginForm />
+            </UserContextProvider>
+        ));
+
+        const emailInput = getByTestId('login-email');
+        const passwordInput = getByTestId('login-password');
+        const submitButton = getByTestId('login-button');
+
+        fireEvent.change(emailInput, { target: { value: 'gg@gg.gg' }});
+        fireEvent.change(passwordInput, { target: { value: '123456' }});
+
+        await waitFor(() => fireEvent.click(submitButton));
+
+        expect(spyApi).toBeCalledTimes(1);
+    });
+
+    it('should not log in - email too small', async () => {
+
+        const spyApi = jest.spyOn(api, 'post');
+
+        const { getByTestId } = await waitFor(() => render(
+            <UserContextProvider>
+                <LoginForm />
+            </UserContextProvider>
+        ));
+
+        const emailInput = getByTestId('login-email');
+        const passwordInput = getByTestId('login-password');
+        const submitButton = getByTestId('login-button');
+
+        fireEvent.change(emailInput, { target: { value: 'g@g.g' }});
+        fireEvent.change(passwordInput, { target: { value: '123456' }});
+
+        await waitFor(() => fireEvent.click(submitButton));
+
+        expect(spyApi).toBeCalledTimes(0);
+        expect(submitButton).toBeDisabled();
+    });
+
+    it('should not log in - password too small', async () => {
+
+        const spyApi = jest.spyOn(api, 'post');
+
+        const { getByTestId } = await waitFor(() => render(
+            <UserContextProvider>
+                <LoginForm />
+            </UserContextProvider>
+        ));
+
+        const emailInput = getByTestId('login-email');
+        const passwordInput = getByTestId('login-password');
+        const submitButton = getByTestId('login-button');
+
+        fireEvent.change(emailInput, { target: { value: 'gg@gg.gg' }});
+        fireEvent.change(passwordInput, { target: { value: '123' }});
+        await waitFor(() => fireEvent.click(submitButton));
+
+        expect(spyApi).toBeCalledTimes(0);
+        expect(submitButton).toBeDisabled();
+    });
+});
--- web/src/components/headerComponents/loginModalComponents/LoginModal/loginModal.test.tsx
+++ web/src/components/headerComponents/loginModalComponents/LoginModal/loginModal.test.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import MockAdapter from 'axios-mock-adapter';
 
 import LoginModal from './index';
 import { UserContextProvider } from '../../../../contexts/userContext';
-import api from '../../../../services/api';
-import { fakeUser, fakeUserJwt } from '../../../../testUtils/fakeData';
 
 describe('Login Modal Tests', () => {
-
-    beforeAll(() => {
-        const apiMock = new MockAdapter(api);
-        apiMock.onPost('/sessions').reply(200, { token: fakeUserJwt });
-        apiMock.onGet('/users/1').reply(200, fakeUser);
-        apiMock.onPost('/users').reply(201, { token: fakeUserJwt });
-    });
 
     it('should render login form by default', async () => {
 
@@ -130,71 +120,7 @@ describe('Login Modal Tests', () => {
         expect(backToLoginAnchor).not.toBeInTheDocument();
     });
 
-    /*it('should log in', async () => {
-
-        const spyApi = jest.spyOn(api, 'post');
-
-        const { getByTestId } = await waitFor(() => render(
-            <UserContextProvider>
-                <LoginModal />
-            </UserContextProvider>
-        ));
-
-        const emailInput = getByTestId('login-email');
-        const passwordInput = getByTestId('login-password');
-        const submitButton = getByTestId('login-button');
-
-        fireEvent.change(emailInput, { target: { value: 'gg@gg.gg' }});
-        fireEvent.change(passwordInput, { target: { value: '123456' }});
-        await waitFor(() => fireEvent.click(submitButton));
-
-        expect(spyApi).toBeCalledTimes(1);
-    });
-
-    it('should not log in - email too small', async () => {
-
-        const spyApi = jest.spyOn(api, 'post');
-
-        const { getByTestId } = await waitFor(() => render(
-            <UserContextProvider>
-                <LoginModal />
-            </UserContextProvider>
-        ));
-
-        const emailInput = getByTestId('login-email');
-        const passwordInput = getByTestId('login-password');
-        const submitButton = getByTestId('login-button');
-
-        fireEvent.change(emailInput, { target: { value: 'g@g.g' }});
-        fireEvent.change(passwordInput, { target: { value: '123456' }});
-        await waitFor(() => fireEvent.click(submitButton));
-
-        expect(spyApi).toBeCalledTimes(0);
-        expect(submitButton).toBeDisabled();
-    });
-
-    it('should not log in - password too small', async () => {
-
-        const spyApi = jest.spyOn(api, 'post');
-
-        const { getByTestId } = await waitFor(() => render(
-            <UserContextProvider>
-                <LoginModal />
-            </UserContextProvider>
-        ));
-
-        const emailInput = getByTestId('login-email');
-        const passwordInput = getByTestId('login-password');
-        const submitButton = getByTestId('login-button');
-
-        fireEvent.change(emailInput, { target: { value: 'gg@gg.gg' }});
-        fireEvent.change(passwordInput, { target: { value: '123' }});
-        await waitFor(() => fireEvent.click(submitButton));
-
-        expect(spyApi).toBeCalledTimes(0);
-        expect(submitButton).toBeDisabled();
-    });
-
+    /*
     it('should create new user', async () => {
 
         const spyApi = jest.spyOn(api, 'post');
