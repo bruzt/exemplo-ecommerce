9123a71136d80709be59e750225a26b825a9c837
Bruno Zutim <bzutim@gmail.com>
2021-09-01 10:01:40 -0300

    refactor: change font color when hover buttons

--- web-admin/src/components/PaginationNav/styles.ts
+++ web-admin/src/components/PaginationNav/styles.ts
@@ -1,40 +1,36 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
+  margin-bottom: 24px;
 
-    margin-bottom: 24px;
+  nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-    nav {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+  nav ul {
+    display: flex;
+    list-style: none;
+  }
+
+  ul li button {
+    margin: 5px;
+    padding: 10px 20px;
+    border: ${(props) => (props.theme.title == "light" ? "1px solid #111" : 0)};
+    border-radius: 2px;
+    font-size: 25px;
+    color: ${(props) => props.theme.primary};
+    cursor: pointer;
+
+    &:hover {
+      background: ${(props) => props.theme.secondary};
+      color: ${(props) => props.theme.color};
     }
 
-    nav ul {
-        display: flex;
-        list-style: none;
+    &.active {
+      background: ${(props) => props.theme.primary};
+      color: ${(props) => props.theme.color};
     }
-
-    ul li button {
-        margin: 5px;
-        padding: 10px 20px;
-        border: ${props => props.theme.title == 'light' ? '1px solid #111' : 0};
-        border-radius: 2px;
-        font-size: 25px;
-        color: ${props => props.theme.primary};
-        cursor: pointer;
-    }
-
-    ul li button:hover {
-        background: ${props => props.theme.secondary};
-    }
-
-    ul li button:active {
-        background: ${props => props.theme.primary};
-    }
-
-    ul li button.active {
-        background: ${props => props.theme.primary};
-        color: ${props => props.theme.color};
-    }
+  }
 `;
