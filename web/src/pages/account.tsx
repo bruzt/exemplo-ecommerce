03cbcfb6b85aeb0b27407774b226432090c35b67
Bruno Zutim <bzutim@gmail.com>
2021-09-08 09:01:16 -0300

    refactor: do not allow access account route if not logged

--- web/src/pages/account.tsx
+++ web/src/pages/account.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import { useRouter } from "next/router";
+import React from "react";
 
-import AccountMenu from '../components/accountComponents/AccountMenu';
+import AccountMenu from "../components/accountComponents/AccountMenu";
+import { useUser } from "../contexts/userContext";
 
 export default function Account() {
+  const userContext = useUser();
+  const router = useRouter();
 
-    return <AccountMenu />
+  if (userContext.getLogin == false) {
+    if (process.browser) router.replace("/");
+
+    return <div></div>;
+  }
+
+  return <AccountMenu />;
 }
