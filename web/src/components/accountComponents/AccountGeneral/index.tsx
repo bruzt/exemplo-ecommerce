d8eb579d025d28415f2c1659f6f1ea3aec415932
Bruno Zutim <bzutim@gmail.com>
2021-09-08 09:08:48 -0300

    refactor: cpf input border to show if cpf is valid or not

--- web/src/components/accountComponents/AccountGeneral/index.tsx
+++ web/src/components/accountComponents/AccountGeneral/index.tsx
@@ -1,209 +1,218 @@
-import React, { useState, useEffect, FormEvent } from 'react';
-import Loading from 'react-loader-spinner';
+import React, { useState, useEffect, FormEvent } from "react";
+import Loading from "react-loader-spinner";
 
-import api from '../../../services/api';
-import formatCpf from '../../../utils/formatCpf';
+import api from "../../../services/api";
+import formatCpf from "../../../utils/formatCpf";
 
-import { useUser } from '../../../contexts/userContext';
+import { useUser } from "../../../contexts/userContext";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 export default function AccountGeneral() {
+  const userContext = useUser();
 
-    const userContext = useUser();
+  const [getName, setName] = useState(userContext.getUser.name || "");
+  const [getEmail, setEmail] = useState(userContext.getUser.email || "");
+  const [getCpf, setCpf] = useState(userContext.getUser.cpf || "");
+  const [getCurrentPassword, setCurrentPassword] = useState("");
+  const [getNewPassword, setNewPassword] = useState("");
+  const [getConfirmNewPassword, setConfirmNewPassword] = useState("");
 
-    const [getName, setName] = useState(userContext.getUser.name || '');
-    const [getEmail, setEmail] = useState(userContext.getUser.email || '');
-    const [getCpf, setCpf] = useState(userContext.getUser.cpf || '');
-    const [getCurrentPassword, setCurrentPassword] = useState('');
-    const [getNewPassword, setNewPassword] = useState('');
-    const [getConfirmNewPassword, setConfirmNewPassword] = useState('');
+  const [getDisabledSubmitButton, setDisabledSubmitButton] = useState(true);
+  const [getIsFetching, setIsFetching] = useState(false);
 
-    const [getDisabledSubmitButton, setDisabledSubmitButton] = useState(true);
-    const [getIsFetching, setIsFetching] = useState(false);
-
-    useEffect(() => {
-        if (
-            (getName.trim() != userContext.getUser.name &&
-                getName.trim().length > 2) ||
-            (getEmail.trim() != userContext.getUser.email &&
-                getEmail.trim().length > 7) ||
-            (getCpf.trim() != userContext.getUser.cpf &&
-                getCpf.trim().length == 11) ||
-            getCurrentPassword.trim().length > 5 &&
-            getNewPassword.trim().length > 5 &&
-            getNewPassword == getConfirmNewPassword
-        ) {
-            setDisabledSubmitButton(false);
-
-        } else {
-            setDisabledSubmitButton(true);
-        }
-    }, [
-        getName,
-        getEmail,
-        getCpf,
-        getCurrentPassword,
-        getNewPassword,
-        getConfirmNewPassword
-    ]);
-
-    function handleSetCpf(event: React.ChangeEvent<HTMLInputElement>) {
-
-        const cpf = String(event.target.value).replace(/\.|-/g, '');
-
-        setCpf(cpf);
+  useEffect(() => {
+    if (
+      (getName.trim() != userContext.getUser.name &&
+        getName.trim().length > 2) ||
+      (getEmail.trim() != userContext.getUser.email &&
+        getEmail.trim().length > 7) ||
+      (getCpf.trim() != userContext.getUser.cpf &&
+        getCpf.trim().length == 11) ||
+      (getCurrentPassword.trim().length > 5 &&
+        getNewPassword.trim().length > 5 &&
+        getNewPassword == getConfirmNewPassword)
+    ) {
+      setDisabledSubmitButton(false);
+    } else {
+      setDisabledSubmitButton(true);
     }
+  }, [
+    getName,
+    getEmail,
+    getCpf,
+    getCurrentPassword,
+    getNewPassword,
+    getConfirmNewPassword,
+  ]);
 
-    async function handleSubmit(event: FormEvent) {
+  function handleSetCpf(event: React.ChangeEvent<HTMLInputElement>) {
+    const cpf = String(event.target.value).replace(/\.|-/g, "");
 
-        event.preventDefault();
+    setCpf(cpf);
+  }
 
-        if (getDisabledSubmitButton || getIsFetching) return;
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
 
-        const data = {
-            name: (getName.trim() == userContext.getUser.name) ? undefined : getName.trim(),
-            email: (getEmail.trim() == userContext.getUser.email) ? undefined : getEmail.trim(),
-            cpf: (getCpf.trim() == userContext.getUser.cpf) ? undefined : getCpf.trim().replace(/\.|-/g, ''),
-            currentPassword: (getCurrentPassword.trim().length == 0) ? undefined : getCurrentPassword.trim(),
-            newPassword: (getNewPassword.trim().length == 0) ? undefined : getNewPassword.trim()
-        }
+    if (getDisabledSubmitButton || getIsFetching) return;
 
-        try {
+    const data = {
+      name:
+        getName.trim() == userContext.getUser.name ? undefined : getName.trim(),
+      email:
+        getEmail.trim() == userContext.getUser.email
+          ? undefined
+          : getEmail.trim(),
+      cpf:
+        getCpf.trim() == userContext.getUser.cpf
+          ? undefined
+          : getCpf.trim().replace(/\.|-/g, ""),
+      currentPassword:
+        getCurrentPassword.trim().length == 0
+          ? undefined
+          : getCurrentPassword.trim(),
+      newPassword:
+        getNewPassword.trim().length == 0 ? undefined : getNewPassword.trim(),
+    };
 
-            setIsFetching(true);
-            await api.put('/users', data);
+    try {
+      setIsFetching(true);
+      await api.put("/users", data);
 
-            const user = { ...userContext.getUser };
-            if (getName != userContext.getUser.name) user.name = data.name;
-            if (getEmail != userContext.getUser.email) user.email = data.email
-            if (getCpf != userContext.getUser.cpf) user.cpf = data.cpf
-            userContext.setUser(user);
-            setCurrentPassword('');
-            setNewPassword('');
-            setConfirmNewPassword('');
-            setIsFetching(false);
-            alert('Dados alterados')
-
-        } catch (error) {
-            console.log(error);
-            console.log(error.response);
-            alert('Erro ao atualizar dados');
-            setIsFetching(false);
-        }
+      const user = { ...userContext.getUser };
+      if (getName != userContext.getUser.name) user.name = data.name;
+      if (getEmail != userContext.getUser.email) user.email = data.email;
+      if (getCpf != userContext.getUser.cpf) user.cpf = data.cpf;
+      userContext.setUser(user);
+      setCurrentPassword("");
+      setNewPassword("");
+      setConfirmNewPassword("");
+      setIsFetching(false);
+      alert("Dados alterados");
+    } catch (error) {
+      console.log(error);
+      console.log(error.response);
+      alert("Erro ao atualizar dados");
+      setIsFetching(false);
     }
+  }
 
-    return (
-        <>
-            <Container data-testid='account-data-component'>
+  return (
+    <>
+      <Container data-testid="account-data-component">
+        {Object.keys(userContext.getUser).length > 0 && (
+          <form onSubmit={handleSubmit}>
+            <h1>Dados da conta</h1>
 
-                {(Object.keys(userContext.getUser).length) > 0 && (
-                    <form
-                        onSubmit={handleSubmit}
-                    >
-                        <h1>Dados da conta</h1>
+            <div className="form-group">
+              <label htmlFor="name">Nome</label>
+              <input
+                type="text"
+                id="name"
+                data-testid="name"
+                value={getName}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
 
-                        <div className="form-group">
-                            <label htmlFor="name">Nome</label>
-                            <input
-                                type="text"
-                                id='name'
-                                data-testid='name'
-                                value={getName}
-                                onChange={(event) => setName(event.target.value)}
-                            />
-                        </div>
+            <div className="form-group">
+              <label htmlFor="email">e-mail</label>
+              <input
+                type="email"
+                data-testid="email"
+                id="email"
+                value={getEmail}
+                onChange={(event) => setEmail(event.target.value.trim())}
+              />
+            </div>
 
-                        <div className="form-group">
-                            <label htmlFor="email">e-mail</label>
-                            <input
-                                type="email"
-                                data-testid='email'
-                                id='email'
-                                value={getEmail}
-                                onChange={(event) => setEmail(event.target.value.trim())}
-                            />
-                        </div>
+            <div className="form-group">
+              <label htmlFor="cpf">CPF</label>
+              <input
+                type="text"
+                id="cpf"
+                data-testid="cpf"
+                maxLength={14}
+                className={`${
+                  getCpf.length > 0 &&
+                  getCpf != userContext.getUser.cpf &&
+                  (formatCpf(getCpf).valid == false ? "invalid" : "valid")
+                }`}
+                value={formatCpf(getCpf).cpf}
+                onChange={handleSetCpf}
+              />
+            </div>
 
-                        <div className="form-group">
-                            <label htmlFor="cpf">CPF</label>
-                            <input
-                                type="text"
-                                id='cpf'
-                                data-testid='cpf'
-                                maxLength={14}
-                                value={formatCpf(getCpf).cpf}
-                                onChange={handleSetCpf}
-                            />
-                        </div>
+            <div className="change-password-group">
+              <h2>Trocar senha</h2>
 
-                        <div className='change-password-group'>
-                            <h2>Trocar senha</h2>
+              <div className="row-group">
+                <div className="form-group">
+                  <label htmlFor="current-password">Senha Atual</label>
+                  <input
+                    type="password"
+                    id="current-password"
+                    data-testid="current-password"
+                    value={getCurrentPassword}
+                    onChange={(event) =>
+                      setCurrentPassword(event.target.value.trim())
+                    }
+                  />
+                </div>
 
-                            <div className='row-group'>
-                                <div className="form-group">
-                                    <label htmlFor="current-password">Senha Atual</label>
-                                    <input
-                                        type="password"
-                                        id='current-password'
-                                        data-testid='current-password'
-                                        value={getCurrentPassword}
-                                        onChange={(event) => setCurrentPassword(event.target.value.trim())}
-                                    />
-                                </div>
+                <div className="form-group">
+                  <label htmlFor="new-password">
+                    Nova senha <span>(no mínimo 6 dígitos)</span>
+                  </label>
+                  <input
+                    type="password"
+                    id="new-password"
+                    data-testid="new-password"
+                    value={getNewPassword}
+                    onChange={(event) =>
+                      setNewPassword(event.target.value.trim())
+                    }
+                  />
+                </div>
 
-                                <div className="form-group">
-                                    <label htmlFor="new-password">
-                                        Nova senha <span>(no mínimo 6 dígitos)</span>
-                                    </label>
-                                    <input
-                                        type="password"
-                                        id='new-password'
-                                        data-testid='new-password'
-                                        value={getNewPassword}
-                                        onChange={(event) => setNewPassword(event.target.value.trim())}
-                                    />
-                                </div>
+                <div className="form-group">
+                  <label htmlFor="confirm-new-password">
+                    Confirmar nova senha
+                  </label>
+                  <input
+                    type="password"
+                    id="confirm-new-password"
+                    data-testid="confirm-new-password"
+                    value={getConfirmNewPassword}
+                    onChange={(event) =>
+                      setConfirmNewPassword(event.target.value.trim())
+                    }
+                  />
+                </div>
+              </div>
+            </div>
 
-                                <div className="form-group">
-                                    <label htmlFor="confirm-new-password">Confirmar nova senha</label>
-                                    <input
-                                        type="password"
-                                        id='confirm-new-password'
-                                        data-testid='confirm-new-password'
-                                        value={getConfirmNewPassword}
-                                        onChange={(event) => setConfirmNewPassword(event.target.value.trim())}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-
-                        <button
-                            type="submit"
-                            disabled={getDisabledSubmitButton || getIsFetching}
-                            className={`${getIsFetching && 'is-fetching'}`}
-                            data-testid='save-button'
-                        >
-                            {getIsFetching
-                                ? (
-                                    <Loading
-                                        type="TailSpin"
-                                        color='#0D2235'
-                                        height='2rem'
-                                        width='2rem'
-                                    />
-
-                                )
-                                : (
-                                    'Salvar'
-                                )
-                            }
-                        </button>
-
-                    </form>
-                )}
-
-            </Container>
-        </>
-    );
+            <button
+              type="submit"
+              disabled={getDisabledSubmitButton || getIsFetching}
+              className={`${getIsFetching && "is-fetching"}`}
+              data-testid="save-button"
+            >
+              {getIsFetching ? (
+                <Loading
+                  type="TailSpin"
+                  color="#0D2235"
+                  height="2rem"
+                  width="2rem"
+                />
+              ) : (
+                "Salvar"
+              )}
+            </button>
+          </form>
+        )}
+      </Container>
+    </>
+  );
 }
--- web/src/components/accountComponents/AccountGeneral/styles.ts
+++ web/src/components/accountComponents/AccountGeneral/styles.ts
@@ -1,92 +1,100 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 1.875rem;
+  }
+
+  form {
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
+    width: 100%;
+    max-width: 37.5rem;
+    background: ${(props) => props.theme.primary};
+    border-radius: 0.3125rem;
+    padding: 1.25rem 0.625rem;
+  }
 
-    h1 {
-        font-size: 1.875rem;
+  div.form-group {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 25rem;
+  }
+
+  label {
+    margin-left: 0.3125rem;
+    margin-top: 1.25rem;
+  }
+
+  label span {
+    font-size: 0.875rem;
+  }
+
+  input {
+    width: 100%;
+    height: 2.5rem;
+    padding: 0.3125rem;
+    font-size: 1.25rem;
+    border: 0;
+    border-radius: 0.3125rem;
+
+    &.invalid {
+      border: 0.125rem solid ${(props) => props.theme.danger};
     }
 
-    form {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        max-width: 37.5rem;
-        background: ${props => props.theme.primary};
-        border-radius: 0.3125rem;
-        padding: 1.25rem 0.625rem;
+    &.valid {
+      border: 0.125rem solid ${(props) => props.theme.success};
+    }
+  }
+
+  div.change-password-group {
+    width: 100%;
+    max-width: 18.75rem;
+    margin-top: 1.25rem;
+  }
+
+  div.change-password-group h2 {
+    margin-bottom: -0.625rem;
+  }
+
+  button[type="submit"] {
+    width: 100%;
+    height: 3.75rem;
+    max-width: 18.75rem;
+    margin-top: 1.875rem;
+    font-size: 1.875rem;
+    padding: 0.3125rem 0.625rem;
+    border: 0;
+    border-radius: 0.3125rem;
+    background: ${(props) => props.theme.success};
+    color: inherit;
+    cursor: pointer;
+
+    &.is-fetching {
+      font-size: 0;
     }
 
-    div.form-group {
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        max-width: 25rem;
+    &:hover {
+      background: ${(props) => props.theme.successActive};
     }
 
-    label {
-        margin-left: 0.3125rem; 
-        margin-top: 1.25rem;
+    &:active {
+      background: ${(props) => props.theme.success};
     }
 
-    label span {
-        font-size: 0.875rem;
+    &:disabled {
+      background: ${(props) => props.theme.danger};
     }
 
-    input {
-        width: 100%;
-        height: 2.5rem;
-        padding: 0.3125rem;
-        font-size: 1.25rem;
-        border: 0;
-        border-radius: 0.3125rem;
+    &:disabled:hover {
+      background: ${(props) => props.theme.dangerActive};
     }
-
-    div.change-password-group {
-        width: 100%;
-        max-width: 18.75rem;
-        margin-top: 1.25rem;
-    }
-
-    div.change-password-group h2 {
-        margin-bottom: -0.625rem;
-    }
-
-    button[type='submit'] {
-        width: 100%;
-        height: 3.75rem;
-        max-width: 18.75rem;
-        margin-top: 1.875rem;
-        font-size: 1.875rem;
-        padding: 0.3125rem 0.625rem;
-        border: 0;
-        border-radius: 0.3125rem;
-        background: ${props => props.theme.success};
-        color: inherit;
-        cursor: pointer;
-
-        &.is-fetching {
-            font-size: 0;
-        }
-
-        &:hover {
-            background: ${props => props.theme.successActive};
-        }
-    
-        &:active {
-            background: ${props => props.theme.success};
-        }
-    
-        &:disabled {
-            background: ${props => props.theme.danger};  
-        }
-    
-        &:disabled:hover {
-            background: ${props => props.theme.dangerActive};  
-        }
-    }
+  }
 `;
