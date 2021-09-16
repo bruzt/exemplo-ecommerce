4c2110615b8cf0dee03154bbb9da68f586f0c5d4
Bruno Zutim <bzutim@gmail.com>
2021-09-16 12:32:07 -0300

    refactor: buyedWith splice

--- backend/src/controllers/productController/show.js
+++ backend/src/controllers/productController/show.js
@@ -1,80 +1,114 @@
-const express = require('express');
-const { Op } = require('sequelize');
+const express = require("express");
+const { Op } = require("sequelize");
 
-const ProductModel = require('../../models/ProductModel');
-const OrdersProductsModel = require('../../models/OrdersProductsModel');
+const ProductModel = require("../../models/ProductModel");
+const OrdersProductsModel = require("../../models/OrdersProductsModel");
 
 function SortIdsByFrequency(arr) {
-    const frequency = arr.reduce((obj, curr) => {
-        obj[curr] = (obj[curr] || 0) + 1;
-        return obj;
-    }, {});
-    const sorted = Object.entries(frequency).sort((a, b) => b[1] - a[1]);
-    const uniqueIds = sorted.map((id) => id[0])
-    return uniqueIds.map(Number);
+  const frequency = arr.reduce((obj, curr) => {
+    obj[curr] = (obj[curr] || 0) + 1;
+    return obj;
+  }, {});
+  const sorted = Object.entries(frequency).sort((a, b) => b[1] - a[1]);
+  const uniqueIds = sorted.map((id) => id[0]);
+  return uniqueIds.map(Number);
 }
 
 /** @param {express.Request} req * @param {express.Response} res */
 module.exports = async (req, res) => {
+  const { id } = req.params;
+  const buyedWith = Number(req.query.buyedWith);
 
-    const { id } = req.params;
-    const buyedWith = Number(req.query.buyedWith);
+  try {
+    const product = await ProductModel.findByPk(id, {
+      attributes: {
+        exclude: ["createdAt", "updatedAt", "deletedAt", "category_id"],
+      },
+      include: [
+        {
+          association: "images",
+          attributes: ["id", "url", "filename"],
+          required: false,
+        },
+        {
+          association: "category",
+          attributes: { exclude: ["createdAt", "updatedAt"] },
+        },
+      ],
+    });
 
-    try {
-        
-        const product = await ProductModel.findByPk(id, {
-            attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt', 'category_id'] },
-            include: [{
-                association: 'images',
-                attributes: ['id', 'url', 'filename'],
-                required: false
-            },
-            {
-                association: 'category',
-                attributes: { exclude: ['createdAt', 'updatedAt'] }
-            }]
-        });
+    if (!product) return res.status(400).json({ message: "product not found" });
 
-        if(!product) return res.status(400).json({ message: 'product not found' });
-        
-        const productWithBuyed = { ...product.get(), productsBuyedWith: [] };
+    const productWithBuyed = { ...product.get(), productsBuyedWith: [] };
 
-        if(buyedWith) {
+    if (buyedWith) {
+      // get ordersProducts who buyed this product
+      const ordersProducts = await OrdersProductsModel.findAll({
+        where: {
+          product_id: product.id,
+        },
+      });
 
-            // get ordersProducts who buyed this product
-            const ordersProducts = await OrdersProductsModel.findAll({
+      // get order ids that have buyed this product
+      const orderIds = ordersProducts.map(
+        (orderProduct) => orderProduct.order_id
+      );
+
+      // get ordersProducts who buyed this product but with other products
+      const ordersProductsByOrderId = await OrdersProductsModel.findAll({
+        where: {
+          order_id: {
+            [Op.in]: orderIds,
+          },
+        },
+      });
+
+      // get products ids
+      const productsIds = ordersProductsByOrderId.map(
+        (orderProductByOrderId) => orderProductByOrderId.product_id
+      );
+
+      // remove this product id
+      const buyedWithProductsIds = productsIds.filter(
+        (productsId) => productsId != product.id
+      );
+
+      // sort by frequency
+      const sortedIds = SortIdsByFrequency(buyedWithProductsIds);
+
+      // get products thas was most buyed with this product
+      const productsBuyedWith = await ProductModel.findAll({
+        where: {
+          id: {
+            [Op.in]: sortedIds.splice(0, buyedWith * 2),
+          },
+          quantity_stock: {
+            [Op.gt]: 0,
+          },
+        },
+        include: [
+          {
+            association: "images",
+            attributes: ["id", "url", "filename"],
+            required: false,
+          },
+          {
+            association: "category",
+            attributes: { exclude: ["createdAt", "updatedAt"] },
+          },
+        ],
+        limit: buyedWith,
+      });
+
+      productsBuyedWith.forEach((product) => {
+        productWithBuyed.productsBuyedWith.push(product);
+      });
+
+      /*const productsBuyedWith = [];
+        for (const sortedId of sortedIds) {
+            const productBuyedWith = await ProductModel.findOne({
                 where: {
-                    product_id: product.id,
-                },
-            });
-    
-            // get order ids that have buyed this product
-            const orderIds = ordersProducts.map((orderProduct) => orderProduct.order_id);
-    
-            // get ordersProducts who buyed this product but with other products
-            const ordersProductsByOrderId = await OrdersProductsModel.findAll({
-                where: {
-                    order_id: {
-                        [Op.in]: orderIds
-                    }
-                },
-            });
-    
-            // get products ids
-            const productsIds = ordersProductsByOrderId.map((orderProductByOrderId) => orderProductByOrderId.product_id);
-    
-            // remove this product id
-            const buyedWithProductsIds = productsIds.filter((productsId) => productsId != product.id);
-    
-            // sort by frequency
-            const sortedIds = SortIdsByFrequency(buyedWithProductsIds);
-    
-            // get products thas was most buyed with this product
-            const productsBuyedWith = await ProductModel.findAll({
-                where: {
-                    id: {
-                        [Op.in]: sortedIds.splice(0,8),
-                    },
+                    id: sortedId,
                     quantity_stock: {
                         [Op.gt]: 0
                     },
@@ -88,41 +122,16 @@ module.exports = async (req, res) => {
                     association: 'category',
                     attributes: { exclude: ['createdAt', 'updatedAt'] }
                 }],
-                limit: buyedWith,
             });
-
-            productsBuyedWith.forEach((product) => {
-                productWithBuyed.productsBuyedWith.push(product);
-            });
-    
-            /*const productsBuyedWith = [];
-            for (const sortedId of sortedIds) {
-                const productBuyedWith = await ProductModel.findOne({
-                    where: {
-                        id: sortedId,
-                        quantity_stock: {
-                            [Op.gt]: 0
-                        },
-                    },
-                    include: [{
-                        association: 'images',
-                        attributes: ['id', 'url', 'filename'],
-                        required: false
-                    },
-                    {
-                        association: 'category',
-                        attributes: { exclude: ['createdAt', 'updatedAt'] }
-                    }],
-                });
-                if (productBuyedWith) productsBuyedWith.push(productBuyedWith);
-                if (productsBuyedWith.length == 4) break;
-            }*/
+            if (productBuyedWith) productsBuyedWith.push(productBuyedWith);
+            if (productsBuyedWith.length == 4) break;
         }
-
-        return res.json(productWithBuyed);
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'internal error' });
+        */
     }
-}
+
+    return res.json(productWithBuyed);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "internal error" });
+  }
+};
--- ts-backend/src/controllers/productController/show.ts
+++ ts-backend/src/controllers/productController/show.ts
@@ -1,80 +1,98 @@
-import { Request, Response } from 'express';
-import { In, MoreThan } from 'typeorm';
+import { Request, Response } from "express";
+import { In, MoreThan } from "typeorm";
 
-import ProductModel from '../../models/ProductModel';
-import OrderProductModel from '../../models/OrderProductModel';
+import ProductModel from "../../models/ProductModel";
+import OrderProductModel from "../../models/OrderProductModel";
 
-interface IProductWithBuyed extends Omit<ProductModel, 'calcIsOnSale'|'calcFinalPrice'|'hasId'|'save'|'remove'|'softRemove'|'recover'|'reload'> {
-    productsBuyedWith: ProductModel[];
+interface IProductWithBuyed
+  extends Omit<
+    ProductModel,
+    | "calcIsOnSale"
+    | "calcFinalPrice"
+    | "hasId"
+    | "save"
+    | "remove"
+    | "softRemove"
+    | "recover"
+    | "reload"
+  > {
+  productsBuyedWith: ProductModel[];
 }
 
 function SortIdsByFrequency(arr: number[]) {
-    const frequency = arr.reduce<{ [key: string]: number }>((obj, curr) => {
-        obj[curr] = (obj[curr] || 0) + 1;
-        return obj;
-    }, {});
-    const sorted = Object.entries(frequency).sort((a, b) => b[1] - a[1]);
-    const uniqueIds = sorted.map((id) => id[0])
-    return uniqueIds.map(Number);
+  const frequency = arr.reduce<{ [key: string]: number }>((obj, curr) => {
+    obj[curr] = (obj[curr] || 0) + 1;
+    return obj;
+  }, {});
+  const sorted = Object.entries(frequency).sort((a, b) => b[1] - a[1]);
+  const uniqueIds = sorted.map((id) => id[0]);
+  return uniqueIds.map(Number);
 }
 
 export default async function show(req: Request, res: Response) {
+  const { id } = req.params;
+  const buyedWith = Number(req.query.buyedWith);
 
-    const { id } = req.params;
-    const buyedWith = Number(req.query.buyedWith);
+  try {
+    const product = await ProductModel.findOne(id, {
+      relations: ["category", "images"],
+    });
 
-    try {
+    if (!product) return res.status(404).json({ message: "product not found" });
 
-        const product = await ProductModel.findOne(id, {
-            relations: ['category', 'images']
-        });
+    const productWithBuyed: IProductWithBuyed = {
+      ...product,
+      productsBuyedWith: [],
+    };
 
-        if (!product) return res.status(404).json({ message: 'product not found' });
+    if (buyedWith) {
+      // get ordersProducts who buyed this product
+      const ordersProducts = await OrderProductModel.find({
+        where: {
+          product_id: product.id,
+        },
+      });
 
-        const productWithBuyed: IProductWithBuyed = { ...product, productsBuyedWith: [] };
+      // get order ids that have buyed this product
+      const orderIds = ordersProducts.map(
+        (orderProduct) => orderProduct.order_id
+      );
 
-        if(buyedWith){
-            // get ordersProducts who buyed this product
-            const ordersProducts = await OrderProductModel.find({
-                where: {
-                    product_id: product.id,
-                },
-            });
-    
-            // get order ids that have buyed this product
-            const orderIds = ordersProducts.map((orderProduct) => orderProduct.order_id);
-    
-            // get ordersProducts who buyed this product but with other products
-            const ordersProductsByOrderId = await OrderProductModel.find({
-                where: {
-                    order_id: In(orderIds),
-                },
-            });
-    
-            // get products ids
-            const productsIds = ordersProductsByOrderId.map((orderProductByOrderId) => orderProductByOrderId.product_id);
-    
-            // remove this product id
-            const buyedWithProductsIds = productsIds.filter((productsId) => productsId != product.id);
-    
-            // sort by frequency
-            const sortedIds = SortIdsByFrequency(buyedWithProductsIds);
-    
-            // get products thas was most buyed with this product
-            const productsBuyedWith = await ProductModel.find({
-                where: {
-                    id: In(sortedIds.splice(0,8)),
-                    quantity_stock: MoreThan(0),
-                },
-                relations: ['category', 'images'],
-                take: buyedWith,
-            });
+      // get ordersProducts who buyed this product but with other products
+      const ordersProductsByOrderId = await OrderProductModel.find({
+        where: {
+          order_id: In(orderIds),
+        },
+      });
 
-            productsBuyedWith.forEach((product) => {
-                productWithBuyed.productsBuyedWith.push(product);
-            });
-    
-            /*const productsBuyedWith: ProductModel[] = [];
+      // get products ids
+      const productsIds = ordersProductsByOrderId.map(
+        (orderProductByOrderId) => orderProductByOrderId.product_id
+      );
+
+      // remove this product id
+      const buyedWithProductsIds = productsIds.filter(
+        (productsId) => productsId != product.id
+      );
+
+      // sort by frequency
+      const sortedIds = SortIdsByFrequency(buyedWithProductsIds);
+
+      // get products thas was most buyed with this product
+      const productsBuyedWith = await ProductModel.find({
+        where: {
+          id: In(sortedIds.splice(0, buyedWith * 2)),
+          quantity_stock: MoreThan(0),
+        },
+        relations: ["category", "images"],
+        take: buyedWith,
+      });
+
+      productsBuyedWith.forEach((product) => {
+        productWithBuyed.productsBuyedWith.push(product);
+      });
+
+      /*const productsBuyedWith: ProductModel[] = [];
             for (const sortedId of sortedIds) {
                 const productBuyedWith = await ProductModel.findOne({
                     where: {
@@ -85,13 +103,13 @@ export default async function show(req: Request, res: Response) {
                 });
                 if (productBuyedWith) productsBuyedWith.push(productBuyedWith);
                 if (productsBuyedWith.length == 4) break;
-            }*/
-        }
-
-        return res.json(productWithBuyed);
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'internal error' });
+            }
+        */
     }
+
+    return res.json(productWithBuyed);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "internal error" });
+  }
 }
