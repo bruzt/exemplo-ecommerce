447094867f3ebcdca9a2356fe5210019c925022c
Bruno Zutim <bzutim@gmail.com>
2021-09-23 09:51:58 -0300

    refactor: remove whitespaces

--- web/src/utils/generateSiteMap.js
+++ web/src/utils/generateSiteMap.js
@@ -1,49 +1,53 @@
 /*eslint no-undef: "off"*/
 
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
 
-async function fetchProducts(){
-    try {
+async function fetchProducts() {
+  try {
+    const response = await axios.get(`${process.env.BACKEND_URL}/products`);
 
-        const response = await axios.get(`${process.env.BACKEND_URL}/products`);
-
-        return response.data.products;
-        
-    } catch (error) {
-        console.error(error);
-    }
+    return response.data.products;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-async function generateSiteMap(){
+async function generateSiteMap() {
+  const products = await fetchProducts();
 
-    const products = await fetchProducts();
+  const date = new Date().toISOString();
 
-    const date = new Date().toISOString();
-
-    const urls = products.map( (product) => (
-        `<url>
-            <loc>${`${process.env.SITE_DOMAIN}/${product.id}?product=${String(product.title).split(' ').join('-')}`}</loc>
+  const urls = products.map(
+    (product) =>
+      `<url>
+            <loc>${`${process.env.SITE_DOMAIN}/${product.id}?product=${String(
+              product.title
+            )
+              .split(" ")
+              .join("-")}`}</loc>
             <lastmod>${date}</lastmod>
             <priority>0.6</priority>
         </url>
         `
-    ));
+  );
 
-    const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             <url>
                 <loc>${process.env.SITE_DOMAIN}</loc>
                 <lastmod>${date}</lastmod>
                 <priority>0.5</priority>
             </url>
-            ${urls.join('')}
+            ${urls.join("")}
         </urlset>
     `;
 
-    fs.writeFileSync('public/robots.txt', `user-agent: *\nsitemap: ${process.env.SITE_DOMAIN}/sitemap.xml`);
-    fs.writeFileSync('public/sitemap.xml', sitemap);
+  fs.writeFileSync(
+    "public/robots.txt",
+    `user-agent: *\nsitemap: ${process.env.SITE_DOMAIN}/sitemap.xml`
+  );
+  fs.writeFileSync("public/sitemap.xml", sitemap.trim());
 }
 
 generateSiteMap();
