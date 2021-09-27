494d05c543946650838e47c943fe5347806303b4
Bruno Zutim <bzutim@gmail.com>
2021-09-27 14:09:00 -0300

    refactor: light theme background

--- web/src/styles/lightTheme.ts
+++ web/src/styles/lightTheme.ts
@@ -1,15 +1,14 @@
-
 export default {
-    title: 'light',
-    background: '#ddd',
-    primary: '#16324C',
-    secondary: '#1C4061',
-    color: '#eee',
-    color2: '#0D2235',
-    success: '#3E8C34',
-    successActive: '#41A933',
-    warning: '#EED202',
-    warningActive: '#f0dc4d',
-    danger: '#a32e39',
-    dangerActive: '#bf2232',
-}
+  title: "light",
+  background: "#587C9D",
+  primary: "#16324C",
+  secondary: "#1C4061",
+  color: "#eee",
+  color2: "#0D2235",
+  success: "#3E8C34",
+  successActive: "#41A933",
+  warning: "#EED202",
+  warningActive: "#f0dc4d",
+  danger: "#a32e39",
+  dangerActive: "#bf2232",
+};
