11fe85a81314302cef5ba142841d8dc91c0738c0
Bruno Zutim <bzutim@gmail.com>
2021-10-05 08:19:47 -0300

    refactor: redis url environment variable

--- backend/src/backgroundJobs/queues.js
+++ backend/src/backgroundJobs/queues.js
@@ -1,49 +1,45 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 let env;
-if(process.env.NODE_ENV === 'production') env = '.env';
-else if(process.env.NODE_ENV === 'test') env = '.env.test';
-else env = '.env.dev';
+if (process.env.NODE_ENV === "production") env = ".env";
+else if (process.env.NODE_ENV === "test") env = ".env.test";
+else env = ".env.dev";
 
 dotenv.config({
-    path: env
+  path: env,
 });
 
 //////////////////////////////////////////////////////////////
 
-const Bull = require('bull');
+const Bull = require("bull");
 
-const sendEmailJob = require('./jobs/sendEmailJob');
+const sendEmailJob = require("./jobs/sendEmailJob");
 
-const redisConfig = {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-};
-
-const sendEmailQueue = new Bull('SendEmailQueue', {
-    redis: redisConfig,
-    limiter: { // envia no maximo 8 emails por minuto
-        max: 8,
-        duration: 60000,
+const sendEmailQueue = new Bull("SendEmailQueue", {
+  redis: process.env.REDIS_URL,
+  limiter: {
+    // envia no maximo 8 emails por minuto
+    max: 8,
+    duration: 60000,
+  },
+  defaultJobOptions: {
+    priority: 2,
+    removeOnComplete: true,
+    // tenta reenviar 3 vezes com um minuto de diferença e falha se demorar mais de 10 segundos
+    attempts: 3,
+    backoff: {
+      type: "fixed",
+      delay: 60000,
     },
-    defaultJobOptions: {
-        priority: 2,
-        removeOnComplete: true,
-        // tenta reenviar 3 vezes com um minuto de diferença e falha se demorar mais de 10 segundos
-        attempts: 3,
-        backoff: { 
-            type: 'fixed',
-            delay: 60000
-        },
-        timeout: 10000,
-    }
+    timeout: 10000,
+  },
 });
 
 exports.sendEmailQueue = sendEmailQueue;
 
 exports.default = [
-    { 
-        queue: sendEmailQueue,
-        process: () => sendEmailQueue.process(sendEmailJob),
-    },
-]
+  {
+    queue: sendEmailQueue,
+    process: () => sendEmailQueue.process(sendEmailJob),
+  },
+];
