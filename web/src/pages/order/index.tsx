58ec80e682816b779dd7434d98f4cd05886b0595
Bruno Zutim <bzutim@gmail.com>
2021-11-14 15:23:31 -0300

    refactor: dont show nothing on first render (before set oderFlowNumber)

--- web/src/pages/order/index.tsx
+++ web/src/pages/order/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useOrder } from "../../contexts/orderContext";
 import { useCart } from "../../contexts/cartContext";
@@ -10,22 +10,25 @@ import SelectAddress from "../../components/orderComponents/SelectAddress";
 import CreateBuyOrder from "../../components/orderComponents/CreateBuyOrder";
 
 export default function Order() {
+  const [getFirstRender, setFirstRender] = useState(true);
+
   const orderContext = useOrder();
   const cartContext = useCart();
 
   useEffect(() => {
     orderContext.setOrderFlowNumber(1);
 
-    return () => orderContext.setOrderFlowNumber(1);
+    setFirstRender(false);
   }, []);
 
   return (
     <PageLayout>
       {cartContext.getCart.length > 0 && <OrderProgress />}
 
-      {orderContext.getOrderFlowNumber == 1 && <Cart />}
-      {orderContext.getOrderFlowNumber == 2 && <SelectAddress />}
-      {orderContext.getOrderFlowNumber == 3 && <CreateBuyOrder />}
+      {getFirstRender == false &&
+        ((orderContext.getOrderFlowNumber == 1 && <Cart />) ||
+          (orderContext.getOrderFlowNumber == 2 && <SelectAddress />) ||
+          (orderContext.getOrderFlowNumber == 3 && <CreateBuyOrder />))}
     </PageLayout>
   );
 }
