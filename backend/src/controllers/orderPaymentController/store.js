83f8b878ea8c4cea979c7d8eb2da307b7373309f
Bruno Zutim <bzutim@gmail.com>
2021-11-15 13:31:53 -0300

    refactor: orderPaymentController internal error with gmt datetime

--- backend/src/controllers/orderPaymentController/store.js
+++ backend/src/controllers/orderPaymentController/store.js
@@ -149,7 +149,7 @@ module.exports = async function store(req, res) {
     if (error.response) {
       console.log(error.response);
     } else {
-      console.log(error);
+      console.error(new Date().toGMTString(), "-", error);
     }
 
     return res.status(500).json({ message: "internal error" });
